36c531638c0df154e6a3d4990bb2fa8a24b674e7
Fionn Malone <f.malone13@imperial.ac.uk>
2017-04-24 13:55:52 -0700

    CPMC WIP.

--- afqmcpy/qmc.py
+++ afqmcpy/qmc.py
@@ -6,28 +6,36 @@ import estimators
 
 def do_qmc(state, interactive=False):
 
-    nw = 10
-    elocal = 0
     est = []
     (eigs, eigv) = scipy.linalg.eig(state.system.T)
-    psi = [walker.Walker(1, state.system, eigv) for w in range(0, nw)]
-    psi_trial = [psi[0].phi[0], psi[0].phi[1]]
+    psi_trial = np.array([eigv[:,:state.system.nup], eigv[:,:state.system.ndown]])
+    idx = eigs.argsort()[::-1]
+    eigs = eigs[idx]
+    eigv = eigv[:,idx]
+    psi = [walker.Walker(1, state.system, psi_trial) for w in range(0, state.nwalkers)]
+    E_T = estimators.local_energy(state.system, psi[0])
     for step in range(0, state.nsteps):
+        elocal = 0.0
+        nw = 0.0
         for w in psi:
-            w.prop_t2(state.projectors.bt2)
-            w.prop_v(state.auxf, state.system.nbasis)
-            w.prop_t2(state.projectors.bt2)
-            w.overlap(psi_trial)
-            # if step%state.nmeasure == 0:
-                # w.reortho()
-            elocal += estimators.local_energy(state.system, w, psi_trial)
+            w.prop_t2(state.projectors.bt2, psi_trial)
+            if w.weight > 0:
+                w.prop_v(state.auxf, state.system.nbasis, psi_trial)
+            # print w.weight
+            if w.weight > 0:
+                w.prop_t2(state.projectors.bt2, psi_trial)
+            # print w.weight
+            if step%state.nmeasure == 0:
+                w.reortho()
+            w.weight = w.weight * np.exp(state.dt*E_T)
+            elocal += w.weight * estimators.local_energy(state.system, w)
+            nw += w.weight
+        E_T = elocal / nw
         if step%state.nmeasure == 0:
             if interactive:
                 est.append(elocal/(state.nmeasure*nw))
             else:
-                print (("%5d     %10.8e")%(step, elocal/(state.nmeasure*nw)))
-            elocal = 0.0
-
+                print (("%5d     %10.8e    %10.8e")%(step, elocal, nw))
 
     if interactive:
         return est
--- afqmcpy/walker.py
+++ afqmcpy/walker.py
@@ -1,32 +1,126 @@
 import numpy as np
 import random
 import scipy.linalg
+import copy
 
 class Walker:
 
-    def __init__(self, nw, system, eigv):
+    def __init__(self, nw, system, trial):
         self.weight = nw
-        self.phi = np.array([eigv[:,:system.nup], eigv[:,:system.ndown]])
-        self.ovlp = ([np.zeros(np.shape(self.phi[0])), 
-                      np.zeros(np.shape(self.phi[0]))])
+        self.phi = copy.deepcopy(trial)
+        self.inv_ovlp = [0, 0]
+        self.inverse_overlap(trial)
+        self.G = [0, 0]
+        self.greens_function(trial)
+        self.ot = 1.0
+
+    def prop_t2(self, bt2, trial):
+        self.phi[0] = bt2.dot(self.phi[0])
+        self.phi[1] = bt2.dot(self.phi[1])
+        # Update inverse overlap
+        self.inverse_overlap(trial)
+        # Update walker weight
+        ot_new = self.calc_otrial(trial)
+        # print self.ot
+        # print self.ot, self.weight, ot_new
+        self.greens_function(trial)
+        if ot_new > 0:
+            self.weight = self.weight * (ot_new/self.ot)
+            self.ot = ot_new
+        else:
+            self.weight = 0.0
 
 
-    def prop_t2(self, bt2):
-        self.phi[0] = np.dot(bt2, self.phi[0])
-        self.phi[1] = np.dot(bt2, self.phi[1])
-
-    def prop_v(self, auxf, nbasis):
+    def prop_v(self, auxf, nbasis, trial):
         # Construct random auxilliary field.
-        x = np.array([auxf[random.randint(0, 1)] for i in range(0, nbasis)])
-        self.phi[0] = np.dot(np.diag(x), self.phi[0])
-        self.phi[1] = np.dot(np.diag(1.0/x), self.phi[1])
+        delta = auxf - 1
+        for i in range(0, nbasis):
+            # Ratio of determinants for the two choices of auxilliary fields
+            probs = 0.5 * np.array([(1+delta[0][0]*self.G[0][i,i])*(1+delta[0][1]*self.G[1][i,i]),
+                                    (1+delta[1][0]*self.G[0][i,i])*(1+delta[1][1]*self.G[1][i,i])])
+            norm = sum(probs)
+            self.weight = self.weight * norm
+            # print "NORM: ", norm, self.weight, probs/norm
+            r = random.random()
+            # print norm
+            # tmp = copy.deepcopy(self.phi[0])
+            # print tmp[i,:]
+            # tmp[i,:] = auxf[0, 0]*tmp[i,:]
+            # print i, auxf[0,0]
+            # print tmp[i,:]
+            # print scipy.linalg.inv(trial[0].T.dot(self.phi[0]))
+            # print self.phi[0]
+            # print trial[0]
+            # print trial[0].T.dot(self.phi)
+            x = copy.deepcopy(self.phi)
+            # print "BEFORE: ", self.ot, 2*probs
+            # print probs[0]/norm, norm
+            if self.weight > 0:
+                # print trial[0]
+                if r < probs[0]/norm:
+                    vtup = self.phi[0][i,:] * delta[0, 0]
+                    vtdown = self.phi[1][i,:] * delta[0, 1]
+                    # print trial[0]
+                    # print self.phi[0][i,:]
+                    # print hex(id(self.phi[0])), hex(id(trial))
+                    self.phi[0][i,:] = self.phi[0][i,:] + vtup
+                    self.phi[1][i,:] = self.phi[1][i,:] + vtdown
+                    self.ot = 2 * self.ot * probs[0]
+                else:
+                    vtup = self.phi[0][i,:] * delta[1, 0]
+                    vtdown = self.phi[1][i,:] * delta[1, 1]
+                    self.phi[0][i,:] = self.phi[0][i,:] + vtup
+                    self.phi[1][i,:] = self.phi[1][i,:] + vtdown
+                    self.ot = 2 * self.ot * probs[1]
+            # print vtup
+            # print self.phi[0][i,:]
+            # print self.phi[0], x + np.array([vtup, [0, 0], [0, 0], [0, 0]])
+            # print (trial[0].T).dot(self.phi[0])
+            # print (trial[0].T).dot(self.phi[0])-x
+            # print np.outer((trial[0].T)[:,i], vtup)
+            # print x
+            # print trial[0]
+            # print trial[0].T.dot(x)
+            # print trial[0].T.dot(x)
+            # print (trial[0].T).dot(self.phi[0])
+            # print trial[0].T.dot(x) + np.outer((trial[0].T)[:,i], vtup)
+            # print scipy.linalg.inv(trial[0].T.dot(x))
+            # print self.inv_ovlp[0]
+            # print scipy.linalg.inv(trial[0].T.dot(self.phi[0]))
+            # print "OVLP: " , self.ot
+            self.inv_ovlp[0] = sherman_morrison(self.inv_ovlp[0], trial[0].T[:,i],
+                                                vtup)
+            self.inv_ovlp[1] = sherman_morrison(self.inv_ovlp[1], trial[1].T[:,i],
+                                                vtdown)
+            self.greens_function(trial)
+            # print "OVLP2: ", ((scipy.linalg.det(trial[0].T.dot(self.phi[0]))*scipy.linalg.det(trial[1].T.dot(self.phi[1]))),
+                    # (scipy.linalg.det(trial[0].T.dot(x[0]))*scipy.linalg.det(trial[1].T.dot(x[1]))))
 
-    def overlap(self, trial):
-        self.ovlp[0] = scipy.linalg.inv(np.dot(np.transpose(trial[0]), self.phi[0]))
-        self.ovlp[1] = scipy.linalg.inv(np.dot(np.transpose(trial[1]), self.phi[1]))
+            # print self.inv_ovlp[0]
+            # print inv[0]
+            # print (self.ot, 1.0/(scipy.linalg.det(inv[0])*scipy.linalg.det(inv[1])),
+                # 1.0/(scipy.linalg.det(self.inv_ovlp[0])*scipy.linalg.det(self.inv_ovlp[1])))
+
+    def inverse_overlap(self, trial):
+        self.inv_ovlp[0] = scipy.linalg.inv(trial[0].T.dot(self.phi[0]))
+        self.inv_ovlp[1] = scipy.linalg.inv(trial[1].T.dot(self.phi[1]))
+
+    def calc_otrial(self, trial):
+        # The importance function, i.e. <phi_T|phi>. We do 1 over this because
+        # inv_ovlp stores the inverse overlap matrix for ease when updating the
+        # green's function.
+        return 1.0/(scipy.linalg.det(self.inv_ovlp[0])*scipy.linalg.det(self.inv_ovlp[1]))
 
     def reortho(self):
-        for (phi, ovlp) in zip(self.phi, self.ovlp):
+        for phi in self.phi:
             (Q, R) = scipy.linalg.qr(phi, mode='economic')
             phi = Q
-            ovlp = ovlp / scipy.linalg.det(R)
+
+    def greens_function(self, trial):
+        self.G[0] = np.dot(np.dot(self.phi[0], self.inv_ovlp[0]), np.transpose(trial[0]))
+        self.G[1] = np.dot(np.dot(self.phi[1], self.inv_ovlp[1]), np.transpose(trial[1]))
+
+
+def sherman_morrison(Ainv, u, vt):
+
+    return Ainv - (Ainv.dot(np.outer(u,vt)).dot(Ainv))/(1.0+vt.dot(Ainv).dot(u))
