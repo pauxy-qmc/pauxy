78d0b04a00b0d47b7d7458a2a9f1e915dda3f91d
Fionn Malone <f.malone13@imperial.ac.uk>
2017-04-24 13:51:47 -0700

    Remove greens function from estimators.

--- afqmcpy/estimators.py
+++ afqmcpy/estimators.py
@@ -1,17 +1,8 @@
 import numpy as np
 
-def greens_function(walker, trial):
+def local_energy(system, walker):
 
-    gup = np.dot(np.dot(walker.phi[0], walker.ovlp[0]), np.transpose(trial[0]))
-    gdown = np.dot(np.dot(walker.phi[1], walker.ovlp[1]), np.transpose(trial[1]))
-
-    return np.array([gup, gdown])
-
-def local_energy(system, walker, trial):
-
-    G = greens_function(walker, trial)
-
-    ke = np.einsum('ij,ji', system.T, G[0]+G[1])
-    pe = sum(system.U*G[0][i][i]*G[1][i][i] for i in range(0, system.nbasis))
+    ke = np.einsum('ij,ji', system.T, walker.G[0]+walker.G[1])
+    pe = sum(system.U*walker.G[0][i][i]*walker.G[1][i][i] for i in range(0, system.nbasis))
 
     return ke + pe
