de0b0540bc21d0f2924f015cd5a65fa10c9b16ef
Fionn Malone <f.malone13@imperial.ac.uk>
2017-04-25 15:46:03 -0700

    Don't use open shell system.

--- tests/cpmc/1d.py
+++ tests/cpmc/1d.py
@@ -6,6 +6,6 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')
 import afqmcpy as af
 
 qs = af.state.State({'name': 'Hubbard', 't': 1.0, 'U': 1, 'nx': 4, 'ny': 1,
-                     'nup': 2, 'ndown': 2}, 0.01, 1, nmeasure=1, nwalkers=1)
+                     'nup': 1, 'ndown': 1}, dt=0.01, nsteps=1000, nmeasure=10, nwalkers=100)
 
 af.qmc.do_qmc(qs)
