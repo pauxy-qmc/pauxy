ffd88660e35ed6148a6571358a25a625c930a585
Fionn Malone <f.malone13@imperial.ac.uk>
2017-04-25 15:44:23 -0700

    Correct energy evaluation.

--- afqmcpy/estimators.py
+++ afqmcpy/estimators.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def local_energy(system, walker):
 
-    ke = np.einsum('ij,ji', system.T, walker.G[0]+walker.G[1])
+    ke = np.sum(system.T * (walker.G[0] + walker.G[1]))
     pe = sum(system.U*walker.G[0][i][i]*walker.G[1][i][i] for i in range(0, system.nbasis))
 
     return ke + pe
