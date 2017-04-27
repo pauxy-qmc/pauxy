39831c7b2a8281e6bbb75f2a70af25a7a7a55d6b
Fionn Malone <f.malone13@imperial.ac.uk>
2017-04-27 16:43:36 -0700

    Update naming of nw to be more consistent with pop control.

--- afqmcpy/qmc.py
+++ afqmcpy/qmc.py
@@ -2,6 +2,7 @@ import numpy as np
 import scipy.linalg
 import walker
 import estimators
+import pop_control
 
 def do_qmc(state, interactive=False):
 
@@ -15,7 +16,7 @@ def do_qmc(state, interactive=False):
     E_T = estimators.local_energy(state.system, psi[0])
     estimators.header()
     elocal = 0
-    nw = 0
+    total_weight = 0
     for step in range(0, state.nsteps):
         for w in psi:
             if w.weight > 0:
@@ -26,16 +27,17 @@ def do_qmc(state, interactive=False):
                 w.prop_t2(state.projectors.bt2, psi_trial)
             w.weight = w.weight * np.exp(state.dt*(E_T-state.cfac))
             elocal += w.weight * estimators.local_energy(state.system, w)
-            nw += w.weight
+            total_weight += w.weight
             if step%state.nmeasure == 0:
                 w.reortho()
         if step%state.nmeasure == 0:
             if interactive:
-                est.append(elocal/(state.nmeasure*nw))
+                est.append(elocal/(state.nmeasure*total_weight))
             else:
-                print (("%9d %10.8e %10.8e %10.8e")%(step, nw/state.nmeasure,
+                print (("%9d %10.8e %10.8e %10.8e")%(step, total_weight/state.nmeasure,
                         elocal/state.nmeasure,np.exp(state.dt*(E_T-state.cfac)) ))
-            E_T = elocal / nw
+            pop_control.comb(psi, state.nwalkers)
+            E_T = elocal / total_weight
             elocal = 0.0
             nw = 0.0
 
