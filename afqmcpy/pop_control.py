3e09ad60c4a643bdbe81a4f0cdd6445bfa2ca33a
Fionn Malone <f.malone13@imperial.ac.uk>
2017-04-28 16:03:28 -0700

    Fix bug with population control.
    copying object member gotcha again. look into python3 list copying and
    profiling.
    
    Remove debug print statements.

--- afqmcpy/pop_control.py
+++ afqmcpy/pop_control.py
@@ -1,8 +1,9 @@
 import numpy as np
 import random
+import copy
 
 def comb(psi, nw):
-    new_psi = 100*[0]
+    new_psi = nw*[0]
     new_ovlps = np.zeros(nw)
     weights = [w.weight for w in psi]
     total_weight = sum(weights)
@@ -12,17 +13,12 @@ def comb(psi, nw):
     # See Booth & Gubernatis PRE 80, 046704 (2009).
     r = random.random()
     comb = [(i+r) * (total_weight/nw) for i in range(nw)]
-    # print comb
-    # print cprobs
     for (ic, c) in enumerate(comb):
         for (iw, w) in enumerate(cprobs):
-            # print iw, w, c
             if c < w:
-                print iw, w, ic, c
-                new_psi[ic] = psi[iw].phi
+                new_psi[ic] = copy.copy(psi[iw].phi)
                 new_ovlps[ic] = psi[iw].ot
                 break
-        # print ic, new_ovlps[ic], len(comb)
 
     # Copy back new information
     for i in range(nw):
