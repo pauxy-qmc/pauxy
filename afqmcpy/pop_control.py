106af031f61126e46c0c367ba3932eccd5d16ae8
Fionn Malone <f.malone13@imperial.ac.uk>
2017-04-28 08:14:36 -0700

    Population control wip.

--- afqmcpy/pop_control.py
+++ afqmcpy/pop_control.py
@@ -2,25 +2,27 @@ import numpy as np
 import random
 
 def comb(psi, nw):
-    new_psi = [ ]
+    new_psi = 100*[0]
     new_ovlps = np.zeros(nw)
     weights = [w.weight for w in psi]
-    print weights
     total_weight = sum(weights)
     cprobs = np.cumsum(weights)
-    print cprobs
 
     # Apply the comb method of population control / branching.
     # See Booth & Gubernatis PRE 80, 046704 (2009).
     r = random.random()
     comb = [(i+r) * (total_weight/nw) for i in range(nw)]
     # print comb
-    # print r
+    # print cprobs
     for (ic, c) in enumerate(comb):
-        for (iw, w) in enumerate(weights):
+        for (iw, w) in enumerate(cprobs):
+            # print iw, w, c
             if c < w:
-                new_psi.append(psi[iw].phi)
+                print iw, w, ic, c
+                new_psi[ic] = psi[iw].phi
                 new_ovlps[ic] = psi[iw].ot
+                break
+        # print ic, new_ovlps[ic], len(comb)
 
     # Copy back new information
     for i in range(nw):
