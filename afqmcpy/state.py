d88f9e6a9b710f9b97840a19b93ba8b05d964a81
Fionn Malone <f.malone13@imperial.ac.uk>
2017-05-02 13:58:14 -0700

    Begin restructure of output.
    Just put everything in a dict...

--- afqmcpy/state.py
+++ afqmcpy/state.py
@@ -1,7 +1,10 @@
-import afqmcpy.hubbard as hubbard
-import numpy as np
 import random
 import json
+import subprocess
+import sys
+import time
+import json
+import afqmcpy.hubbard as hubbard
 import afqmcpy.trial_wave_function as trial_wave_function
 
 class State:
@@ -29,12 +32,46 @@ class State:
 
         (self.psi_trial, self.sp_eigs) = trial_wave_function.free_electron(self.system)
         random.seed(qmc_opts['rng_seed'])
+        self.pack_metadata
         # Handy to keep original dicts so they can be printed at run time.
         self.model = model
         self.qmc_opts = qmc_opts
 
+
     def write_json(self):
 
+        # Combine some metadata in dicts so it can be easily printed/read.
+        calc_info =  {'sha1': get_git_revision_hash(),
+                      'Run time': time.asctime()}
+        derived = {'sp_eigv': self.sp_eigs}
         print ("# Input options: ")
-        print (json.dumps({'model': self.model, 'qmc_options': self.qmc_opts}, indent=4))
+        print (json.dumps({'calculation': calc_info,
+                           'derived': derived,
+                           'model': self.model,
+                           'qmc_options': self.qmc_opts}, indent=4))
         print ("# End of input options ")
+
+
+def get_git_revision_hash():
+    '''Return git revision.
+
+    Adapted from: http://stackoverflow.com/questions/14989858/get-the-current-git-hash-in-a-python-script
+
+Returns
+-------
+sha1 : string
+    git hash with -dirty appended if uncommitted changes.
+'''
+
+    src = [s for s in sys.path if 'afqmcpy' in s][0]
+
+    sha1 = subprocess.check_output(['git', 'rev-parse', 'HEAD'],
+                                   cwd=src).strip()
+    suffix = subprocess.check_output(['git', 'status',
+                                     '--porcelain',
+                                     'afqmcpy/'],
+                                     cwd=src).strip()
+    if suffix:
+        return sha1.decode('utf-8') + '-dirty'
+    else:
+        return sha1.decode('utf-8')
