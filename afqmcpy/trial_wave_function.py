c8cdae869d5443071d723c17529852de34483c49
Fionn Malone <f.malone13@imperial.ac.uk>
2017-05-12 17:53:58 -0700

    Type wavefunction.

--- afqmcpy/trial_wave_function.py
+++ afqmcpy/trial_wave_function.py
@@ -1,13 +1,17 @@
 import scipy.linalg
 import numpy
 
-def free_electron(system):
+def free_electron(system, cplx):
 
     (eigs, eigv) = scipy.linalg.eigh(system.T)
     idx = eigs.argsort()
     eigs = eigs[idx]
     eigv = eigv[:,idx]
+    if cplx:
+        trial_type = complex
+    else:
+        trial_type = float
     psi_trial = numpy.array([eigv[:,:system.nup], eigv[:,:system.ndown]],
-                            dtype=complex)
+                            dtype=trial_type)
 
     return (psi_trial, eigs)
