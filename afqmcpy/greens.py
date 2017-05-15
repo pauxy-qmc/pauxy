066b247b3ce3d8d60cc7e772e805231c53d78e56
Fionn Malone <f.malone13@imperial.ac.uk>
2017-05-15 14:00:47 -0700

    Remove redundant module.

--- afqmcpy/greens.py
+++
@@ -1,6 +0,0 @@
-def greens_function(walker, trial):
-
-    gup = np.dot(np.dot(walker.phi[0], walker.ovlp[0]), np.transpose(trial[0]))
-    gdown = np.dot(np.dot(walker.phi[1], walker.ovlp[1]), np.transpose(trial[1]))
-
-    return np.array(gup, gdown)
