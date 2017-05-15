c9f39a7eb0be5f0e10686558c5ab3bfe11a80419
Fionn Malone <f.malone13@imperial.ac.uk>
2017-05-15 14:01:40 -0700

    Use numpy random number generator and numpy.

--- afqmcpy/pop_control.py
+++ afqmcpy/pop_control.py
@@ -1,17 +1,16 @@
-import numpy as np
-import random
+import numpy
 import copy
 
 def comb(psi, nw):
     new_psi = nw*[0]
-    new_ovlps = np.zeros(nw)
+    new_ovlps = numpy.zeros(nw)
     weights = [w.weight for w in psi]
     total_weight = sum(weights)
-    cprobs = np.cumsum(weights)
+    cprobs = numpy.cumsum(weights)
 
     # Apply the comb method of population control / branching.
     # See Booth & Gubernatis PRE 80, 046704 (2009).
-    r = random.random()
+    r = numpy.random.random()
     comb = [(i+r) * (total_weight/nw) for i in range(nw)]
     for (ic, c) in enumerate(comb):
         for (iw, w) in enumerate(cprobs):
