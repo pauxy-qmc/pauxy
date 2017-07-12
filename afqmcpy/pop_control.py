1b25174431d3c57cc884e4e0a22d5ca983a3f335
Fionn Malone <f.malone13@imperial.ac.uk>
2017-07-12 16:14:41 -0700

    Avoid deepcopy of psi_history which is not necessary.

--- afqmcpy/pop_control.py
+++ afqmcpy/pop_control.py
@@ -2,13 +2,12 @@ import numpy
 import copy
 
 def comb(psi, nw, psi_history=None):
+    # Need make a copy to since the elements in psi are only references to
+    # walker objects in memory. We don't want future changes in a given element
+    # of psi having unintended consequences.
     new_psi = copy.deepcopy(psi)
-    if psi_history is not None:
-        # ugh
-        new_history = copy.deepcopy(psi_history)
     weights = [w.weight for w in psi]
     parent_ix = numpy.arange(len(psi))
-    parent_link = numpy.arange(len(psi))
     total_weight = sum(weights)
     cprobs = numpy.cumsum(weights)
 
@@ -24,7 +23,19 @@ def comb(psi, nw, psi_history=None):
 
     # Copy back new information
     for (i,p) in enumerate(parent_ix):
+        # Todo: look at what we actually need to copy.
         psi[i] = copy.deepcopy(new_psi[p])
         psi[i].weight = 1.0
-        if psi_history is not None:
-            psi_history[i,:] = copy.deepcopy(new_history[p,:])
+    # Need to be a bit careful here. When we perform population control by
+    # updating psi_history using the population control map we are only copying
+    # references to the walkers which are stored in memory. Since these are not
+    # modified anywhere else in the code (we deepcopy historic wavefunctions
+    # into psi_history in the first place) we don't need to worry about any
+    # modifications to walkers being propagated through to other walkers with a
+    # common ancestor. This only serves to copy the parent's field history up
+    # to this point in the simulation if branching/death has occured by copying
+    # the reference to the parent's walker object in memory and thus avoids an
+    # expensive deepcopy operation.
+    if psi_history is not None:
+        psi_history = psi_history[parent_ix]
+    return psi_history
