4dd6d617d3c75dde483ac38a142a347ad108e0ab
Fionn Malone <f.malone13@imperial.ac.uk>
2017-07-28 16:59:51 -0700

    Add docstring.

--- afqmcpy/state.py
+++ afqmcpy/state.py
@@ -108,9 +108,66 @@ class State:
         print_function(output_string)
 
 class QMCOpts:
+    """Input options and certain constants / parameters derived from them.
+
+    Initialised from a dict containing the following options, not all of which
+    are required.
+
+    Parameters/Attributes
+    ---------------------
+    method : string
+        Which auxiliary field method are we using? Currently only CPMC is
+        implemented.
+    nwalkers : int
+        Number of walkers to propagate in a simulation.
+    dt : float
+        Timestep.
+    nsteps : int
+        Total number of Monte Carlo steps to perform.
+    nmeasure : int
+        Frequency of energy measurements.
+    nstblz : int
+        Frequency of Gram-Schmidt orthogonalisation steps.
+    npop_control : int
+        Frequency of population control.
+    temp : float
+        Temperature. Currently not used.
+    nequilibrate : int
+        Number of steps used for equilibration phase. Only used to fix local
+        energy bound when using phaseless approximation.
+    importance_sampling : boolean
+        Are we using importance sampling. Default True.
+    hubbard_statonovich : string
+        Which hubbard stratonovich transformation are we using. Currently the
+        options are:
+            discrete : Use the discrete Hirsch spin transformation.
+            opt_continuous : Use the continuous transformation for the Hubbard
+                model.
+            generic : Use the generic transformation. Not currently implemented.
+    ffts : boolean
+        Use FFTS to diagonalise the kinetic energy propagator? Default False.
+        This may speed things up for larger lattices.
+
+    Derived Attributes
+    ------------------
+    cplx : boolean
+        Do we require complex wavefunctions?
+    mf_shift : float
+        Mean field shift for continuous Hubbard-Stratonovich transformation.
+    iut_fac : complex float
+        Stores i*(U*dt)**0.5 for continuous Hubbard-Stratonovich transformation.
+    ut_fac : float
+        Stores (U*dt) for continuous Hubbard-Stratonovich transformation.
+    mf_nsq : float
+        Stores M * mf_shift for continuous Hubbard-Stratonovich transformation.
+    local_energy_bound : float
+        Energy pound for continuous Hubbard-Stratonovich transformation.
+    mean_local_energy : float
+        Estimate for mean energy for continuous Hubbard-Stratonovich transformation.
+    """
 
     def __init__(self, inputs, system):
-        self.method = inputs.get('method', None)
+        self.method = inputs.get('method', 'CPMC')
         self.nwalkers = inputs.get('nwalkers', None)
         self.dt = inputs.get('dt', None)
         self.nsteps = inputs.get('nsteps', None)
