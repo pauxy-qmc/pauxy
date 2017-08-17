e3d2669ab29999a4c29b366a55e0ff875f555dad
Fionn Malone <f.malone13@imperial.ac.uk>
2017-08-16 17:56:15 -0700

    Correct bisection script.

--- tools/bisect.sh
+++ tools/bisect.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-../../testcode/bin/testcode.py
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+echo $parent_path
+cd "$parent_path"/..
+pwd
+cd tests
+pwd
+../../testcode/bin/testcode.py -c continuous
