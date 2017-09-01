b73b94fe9a7871171af6355a1ef035ad466491ca
Fionn Malone <f.malone13@imperial.ac.uk>
2017-09-01 12:08:51 -0700

    Consistent docstring formatting.

--- afqmcpy/utils.py
+++ afqmcpy/utils.py
@@ -4,22 +4,22 @@ import numpy
 import scipy.linalg
 
 def sherman_morrison(Ainv, u, vt):
-    '''Sherman-Morrison update of a matrix inverse
+    """Sherman-Morrison update of a matrix inverse
 
-Parameters
-----------
-Ainv : numpy.ndarray
-    Matrix inverse of A to be updated.
-u : numpy.array
-    column vector
-vt : numpy.array
-    transpose of row vector
+    Parameters
+    ----------
+    Ainv : numpy.ndarray
+        Matrix inverse of A to be updated.
+    u : numpy.array
+        column vector
+    vt : numpy.array
+        transpose of row vector
 
-Returns
--------
-Ainv : numpy.ndarray
-    Update inverse: :math:`(A + u x vT)^{-1}`
-'''
+    Returns
+    -------
+    Ainv : numpy.ndarray
+        Update inverse: :math:`(A + u x vT)^{-1}`
+    """
 
     return (
         Ainv - (Ainv.dot(numpy.outer(u,vt)).dot(Ainv))/(1.0+vt.dot(Ainv).dot(u))
@@ -27,23 +27,23 @@ Ainv : numpy.ndarray
 
 
 def diagonalise_sorted(H):
-    '''Diagonalise Hermitian matrix H and return sorted eigenvalues and vectors.
+    """Diagonalise Hermitian matrix H and return sorted eigenvalues and vectors.
 
     Eigenvalues are sorted as e_1 < e_2 < .... < e_N, where H is an NxN
     Hermitian matrix.
 
-Parameters
-----------
-H : :class:`numpy.ndarray`
-    Hamiltonian matrix to be diagonalised.
+    Parameters
+    ----------
+    H : :class:`numpy.ndarray`
+        Hamiltonian matrix to be diagonalised.
 
-Returns
--------
-eigs : :class:`numpy.array`
-    Sorted eigenvalues
-eigv :  :class:`numpy.array`
-    Sorted eigenvectors (same sorting as eigenvalues).
-'''
+    Returns
+    -------
+    eigs : :class:`numpy.array`
+        Sorted eigenvalues
+    eigv :  :class:`numpy.array`
+        Sorted eigenvectors (same sorting as eigenvalues).
+    """
 
     (eigs, eigv) = scipy.linalg.eigh(H)
     idx = eigs.argsort()
