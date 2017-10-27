9bc15c2009216d1fda167725f0fb21c41084855f
Fionn Malone <f.malone13@imperial.ac.uk>
2017-10-27 11:48:30 -0700

    Fix closing of hdf5 output file.

--- afqmcpy/initialise.py
+++ afqmcpy/initialise.py
@@ -105,4 +105,4 @@ def finalise(state, init_time):
         print ("# End Time: %s"%time.asctime())
         print ("# Running time : %.6f seconds"%(time.time()-init_time))
         if state.estimators.back_propagation:
-            state.estimators.back_prop.h5f.close()
+            state.estimators.h5f.close()
