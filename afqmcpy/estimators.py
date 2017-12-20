35847af2ef22282841821accdb26c9dd6f6efc8f
Fionn Malone <f.malone13@imperial.ac.uk>
2017-12-20 14:16:08 -0800

    Allow for output filename for estimators.

--- afqmcpy/estimators.py
+++ afqmcpy/estimators.py
@@ -85,12 +85,14 @@ class Estimators:
             print_key(self.key)
             print_header(self.header)
             index = estimates.get('index', 0)
-            overwrite = estimates.get('overwrite', True)
-            h5f_name =  'estimates.%s.h5'%index
-            while os.path.isfile(h5f_name) and not overwrite:
-                index = int(h5f_name.split('.')[1])
-                index = index + 1
+            h5f_name = estimates.get('filename', None)
+            if h5f_name is None:
+                overwrite = estimates.get('overwrite', True)
                 h5f_name =  'estimates.%s.h5'%index
+                while os.path.isfile(h5f_name) and not overwrite:
+                    index = int(h5f_name.split('.')[1])
+                    index = index + 1
+                    h5f_name =  'estimates.%s.h5'%index
             self.h5f = h5py.File(h5f_name, 'w')
             self.h5f.create_dataset('metadata',
                                     data=numpy.array([json_string], dtype=object),
