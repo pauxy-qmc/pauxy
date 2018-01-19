880afcb11ca4758803c37988ff9d45088fb1682f
Fionn Malone <f.malone13@imperial.ac.uk>
2018-01-19 14:45:33 -0800

    Calculate modified green's function (theta).
    Does not include final trial wavefunction factor which is used to
    precomputed some quantities elsewhere.

--- afqmcpy/estimators.py
+++ afqmcpy/estimators.py
@@ -866,6 +866,41 @@ def gab(A, B):
     GAB = B.dot(inv_O.dot(A.conj().T))
     return GAB
 
+def gab_mod(A, B):
+    r"""One-particle Green's function.
+
+    This actually returns 1-G since it's more useful, i.e.,
+
+    .. math::
+        \langle \phi_A|c_i^{\dagger}c_j|\phi_B\rangle =
+        [B(A^{\dagger}B)^{-1}A^{\dagger}]_{ji}
+
+    where :math:`A,B` are the matrices representing the Slater determinants
+    :math:`|\psi_{A,B}\rangle`.
+
+    For example, usually A would represent (an element of) the trial wavefunction.
+
+    .. warning::
+        Assumes A and B are not orthogonal.
+
+    Parameters
+    ----------
+    A : :class:`numpy.ndarray`
+        Matrix representation of the bra used to construct G.
+    B : :class:`numpy.ndarray`
+        Matrix representation of the ket used to construct G.
+
+    Returns
+    -------
+    GAB : :class:`numpy.ndarray`
+        (One minus) the green's function.
+    """
+    # Todo: check energy evaluation at later point, i.e., if this needs to be
+    # transposed. Shouldn't matter for Hubbard model.
+    inv_O = scipy.linalg.inv((A.conj().T).dot(B))
+    GAB = B.dot(inv_O)
+    return GAB
+
 def gab_multi_det(A, B, coeffs):
     r"""One-particle Green's function.
 
