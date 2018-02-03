68b407449bd9801a1da24d4fda5482993914c217
Fionn Malone <f.malone13@imperial.ac.uk>
2018-02-03 14:32:26 -0800

    Remove redundant options.

--- tools/reblock.py
+++ tools/reblock.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
-'''Run a reblocking analysis on AFQMCPY QMC output files. Heavily adapted from
-HANDE'''
-
+'''Simple analysis of PAUXY QMC output files.'''
 import argparse
 import os
 import sys
@@ -9,8 +7,7 @@ import pandas as pd
 import json
 _script_dir = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(os.path.join(_script_dir, 'analysis'))
-import analysis.blocking
-import matplotlib.pyplot as plt
+import pauxy.analysis.blocking
 import glob
 
 
@@ -38,20 +35,8 @@ start_iteration : int
     parser.add_argument('-s', '--start', type=int, dest='start_time',
                         default=None, help='Imaginary time after which we '
                         'gather statistics.  Default: 0')
-    parser.add_argument('-v', '--verbose', dest='verbose', action='store_true',
-                        default=False, help='Increase verbosity of output.')
-    parser.add_argument('-l', '--loops', dest='loops', action='store_true',
-                        default=False, help='Average over independent simulations.')
-    parser.add_argument('-t', '--tail', dest='tail', action='store_true',
-                        default=False, help='Short output.')
-    parser.add_argument('-i', '--input', dest='input', action='store_true',
-                        default=False, help='Extract input file.')
     parser.add_argument('-c', '--correlation', dest='cfunc', action='store_true',
                         default=False, help='Extract correlation functions.')
-    parser.add_argument('-e', '--estimates', dest='estimates', action='store_true',
-                        default=False, help='Analyse all estimators in hdf5 output.')
-    parser.add_argument('-p', '--plot', dest='plot', action='store_true',
-                        default=False, help='Make plots.')
     parser.add_argument('-f', nargs='+', dest='filenames',
                         help='Space-separated list of files to analyse.')
 
@@ -80,41 +65,14 @@ None.
 '''
 
     options = parse_args(args)
-    if options.estimates:
-        if '*' in options.filenames[0]:
-            files = glob.glob(options.filenames[0])
-        else:
-            files = options.filenames
-        (bp_av, norm) = analysis.blocking.analyse_estimates(files,
-                                                    start_time=options.start_time,
-                                                    multi_sim=options.loops,
-                                                    cfunc=options.cfunc)
-        if options.plot:
-            fig, ax = plt.subplots(2, sharex=True)
-            ax[0].errorbar(data.nbp.values, data['T'].values,
-                    yerr=data.T_error.values, fmt='o')
-            ax[1].errorbar(data.nbp.values, data.V.values,
-                    data.V_error.values, fmt='o')
-            ax[0].set_ylabel('T')
-            ax[1].set_ylabel('V')
-            plt.savefig('conv_bp.pdf', fmt='pdf')
-            plt.show()
-    elif options.loops:
-        data = analysis.blocking.average_tau(options.filenames)
-        if options.tail:
-            print (data.tail(1).to_string(index=False))
-        else:
-            print (data.to_string(index=False))
-    elif options.input:
-        metadata = analysis.extraction.extract_data_sets(options.filenames)[0][0]
-        print (json.dumps(metadata, sort_keys=False, indent=4))
+    if '*' in options.filenames[0]:
+        files = glob.glob(options.filenames[0])
     else:
-        (reblock, summary) = analysis.blocking.run_blocking_analysis(options.filenames, options.start_iteration)
-        if options.verbose:
-            print (reblock)
-        else:
-            print (summary)
-
+        files = options.filenames
+    (bp_av, norm) = analysis.blocking.analyse_estimates(files,
+                                                start_time=options.start_time,
+                                                multi_sim=options.loops,
+                                                cfunc=options.cfunc)
 if __name__ == '__main__':
 
     main(sys.argv[1:])
