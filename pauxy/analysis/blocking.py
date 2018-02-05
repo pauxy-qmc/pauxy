12386d47a0e32f690723c2209c25d31511aa9f2b
Fionn Malone <f.malone13@imperial.ac.uk>
2018-02-05 09:59:46 -0800

    Some uniqueness for analysed data file.

--- pauxy/analysis/blocking.py
+++ pauxy/analysis/blocking.py
@@ -154,7 +154,11 @@ def analyse_estimates(files, start_time=0, multi_sim=False, cfunc=False):
             itcfk_data.append(itcfk[skip:nzero])
         mds.append(json.dumps(m))
 
-    store = h5py.File('analysed_estimates.h5', 'w')
+    file_info = files[0].split('.')
+    ix = file_info[-2]
+    base = file_info[0]
+    outfile = 'analysed_' + base + '.' + ix + '.h5'
+    store = h5py.File(outfile, 'w')
     store.create_dataset('metadata', data=numpy.array(mds, dtype=object),
                          dtype=h5py.special_dtype(vlen=str))
     if itcf is not None:
