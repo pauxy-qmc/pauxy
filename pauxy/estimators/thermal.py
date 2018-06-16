cb6e9f86f86e18ef42091d23cd27a3789b3cedf0
Fionn Malone <f.malone13@imperial.ac.uk>
2018-06-16 13:35:29 -0700

    Use one rdm rather than G.

--- pauxy/estimators/thermal.py
+++ pauxy/estimators/thermal.py
@@ -3,9 +3,14 @@ import scipy.linalg
 
 
 def greens_function(A):
-    I = numpy.identity(A[0].shape[0])
+    I = numpy.identity(A.shape[-1])
     return numpy.array([scipy.linalg.inv(I+A[0]), scipy.linalg.inv(I+A[1])])
 
-def particle_number(G):
-    nav = G[0].trace() + G[1].trace()
+def one_rdm(A):
+    I = numpy.identity(A.shape[-1])
+    G = greens_function(A)
+    return numpy.array([I-G[0].T, I-G[1].T])
+
+def particle_number(dmat):
+    nav = dmat[0].trace() + dmat[1].trace()
     return nav
