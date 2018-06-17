340856ccbf7e6bccf3ce7a9be38eee9832ffb2f2
Fionn Malone <f.malone13@imperial.ac.uk>
2018-06-16 18:05:47 -0700

    Fix propagator.

--- pauxy/propagation/hubbard.py
+++ pauxy/propagation/hubbard.py
@@ -416,6 +416,7 @@ class ThermalDiscrete(object):
         self.auxf = self.auxf * numpy.exp(-0.5*qmc.dt*system.U)
         self.delta = self.auxf - 1
         self.BH1 = trial.dmat
+        self.BT_BP = trial.dmat
         self.BH1_inv = trial.dmat_inv
         self.BV = numpy.zeros((2,trial.dmat.shape[-1]), dtype=trial.dmat.dtype)
         if self.free_projection:
@@ -429,9 +430,9 @@ class ThermalDiscrete(object):
     def update_greens_function(self, walker, i, xi):
         for spin in [0,1]:
             g = walker.G[spin,:,i]
-            denom = 1 + (1-g[i])*delta[xi, spin]
+            denom = 1 + (1-g[i])*self.delta[xi, spin]
             walker.G[spin] = (
-                walker.G[spin] + self.delta[ix,spin]*numpy.einsum('i,j->ij', g, 1-g) / denom
+                walker.G[spin] + self.delta[xi,spin]*numpy.einsum('i,j->ij', g, 1-g) / denom
             )
     def propagate_greens_function(self, walker):
         walker.G[0] = self.BH1[0].dot(walker.G[0]).dot(self.BH1_inv[0])
@@ -444,7 +445,7 @@ class ThermalDiscrete(object):
         R2_dn = (1+(1-walker.G[1,i,i])*(self.delta[1,1]-1))
         return 0.5 * numpy.array([R1_up*R1_dn, R2_up*R2_dn])
 
-    def propagate_walker_constrained(self, walker, time_slice):
+    def propagate_walker_constrained(self, system, walker, time_slice):
         for i in range(0, system.nbasis):
             probs = self.calculate_overlap_ratio(walker, i)
             phaseless_ratio = numpy.maximum(probs.real, [0,0])
