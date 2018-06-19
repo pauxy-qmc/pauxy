d7ba76451528bc45644119a639b0d1f6301baf60
Joonho Lee <joonholee@c-76-102-204-115.hsd1.ca.comcast.net>
2018-06-18 21:04:28 -0700

    faster local energy for ueg

--- pauxy/estimators/ueg.py
+++ pauxy/estimators/ueg.py
@@ -8,7 +8,6 @@ except ImportError:
 import scipy.linalg
 
 def local_energy_ueg(system, G):
-    # start = time.time()
     
     ke = numpy.sum(system.T[0] * G[0] + system.T[1] * G[1]) # kinetic energy
     
@@ -21,59 +20,61 @@ def local_energy_ueg(system, G):
     G[0] = G[0].T
     G[1] = G[1].T
 
-    # print (G[0])
+#   Todo: make it work for different spin
+    kf = system.basis[0:ne[0]]
+
+    ikpq = []
+    for (iq, q) in enumerate(system.qvecs):
+        idxkpq =[]
+        for i, k in enumerate(kf):
+            kpq = k + q
+            idx = system.lookup_basis(kpq)
+            if idx is not None:
+                idxkpq += [(i,idx)]
+        ikpq += [idxkpq]
+
+    # print(ikpq)
     # exit()
 
+    ipmq = []
+    for (iq, q) in enumerate(system.qvecs):
+        idxpmq =[]
+        for i, p in enumerate(kf):
+            pmq = p - q
+            idx = system.lookup_basis(pmq)
+            if idx is not None:
+                idxpmq += [(i,idx)]
+        ipmq += [idxpmq]
+
     for s in [0, 1]:
-        kf = system.basis[0:ne[s]]
         for (iq, q) in enumerate(system.qvecs):
-            # for i, k in enumerate(system.basis):
-            for i, k in enumerate(kf):
-                kpq = k + q
-                ikpq = system.lookup_basis(kpq)
-                if ikpq is not None:
-                    Gkpq[s][iq] += G[s][ikpq,i]
-                    # if (abs(Gkpq[i]) > 1e-16):
-                        # print ("NZ: ", i, k, q, kpq, ikpq)
-                    # for (j, p) in enumerate(system.basis):
-                    for (j, p) in enumerate(kf):
-                        pmq = p - q
-                        # if (kpq.dot(kpq) == 0): print ("kpq: ", ikpq, i, k, j, p, q, pmq)
-                        ipmq = system.lookup_basis(pmq)
-                        if ipmq is not None:
-                            # if (numpy.all(k==[0,0,1])):
-                                # print (i, k, iq, kpq, kpq.dot(kpq), ikpq,
-                                        # system.lookup_basis([0,0,0]),
-                                        # system.map_basis_to_index([0,0,0]),
-                                        # G[0][ikpq,j], G[0][ipmq,i])
-                            Gprod[s][iq] += G[s][ikpq,j]*G[s][ipmq,i]
-                pmq = k - q
-                ipmq = system.lookup_basis(pmq)
-                if ipmq is not None:
-                    Gpmq[s][iq] += G[s][ipmq,i]
+            for (i, idxkpq) in ikpq[iq]:
+                Gkpq[s][iq] += G[s][idxkpq,i]
+                for (j,idxpmq) in ipmq[iq]:
+                    Gprod[s][iq] += G[s][idxkpq,j]*G[s][idxpmq,i]
+
+        for (iq, q) in enumerate(system.qvecs):
+            for (j,idxpmq) in ipmq[iq]:
+                Gpmq[s][iq] += G[s][idxpmq,j]
 
     essa = (1.0/(2.0*system.vol))*system.vqvec.dot(Gkpq[0]*Gpmq[0]-Gprod[0])
     essb = (1.0/(2.0*system.vol))*system.vqvec.dot(Gkpq[1]*Gpmq[1]-Gprod[1])
     eos = 0.5*((1.0/system.vol)*system.vqvec.dot(Gkpq[0]*Gpmq[1]) + (1.0/system.vol)*system.vqvec.dot(Gkpq[1]*Gpmq[0]))
 
-    # print("essa = %10.5f, essb = %10.5f, eos = %10.5f"%(essa, essb, eos))
-
     pe = essa + essb + eos
-    
+
     G[0] = G[0].T
     G[1] = G[1].T
-    # end = time.time()
-    # print("local_energy_ueg (seconds) %10.5f"%(end - start))
 
     return (ke+pe, ke, pe)
 
 def unit_test():
     from pauxy.systems.ueg import UEG
     import numpy as np
-    inputs = {'nup':10, 
-    'ndown':10,
+    inputs = {'nup':7, 
+    'ndown':7,
     'rs':1.0,
-    'ecut':2.5}
+    'ecut':1.0}
     system = UEG(inputs, True)
     nbsf = system.nbasis
     Pa = np.zeros([nbsf,nbsf])
