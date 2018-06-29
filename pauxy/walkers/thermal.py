ae6c411a3521d6cfc7df7fa095c9a481f225c893
linusjoonho <linusjoonho@gmail.com>
2018-06-28 17:58:54 -0700

    qr implementation

--- pauxy/walkers/thermal.py
+++ pauxy/walkers/thermal.py
@@ -1,4 +1,5 @@
 import copy
+import cmath
 import numpy
 import scipy.linalg
 from pauxy.estimators.thermal import greens_function, one_rdm_from_G
@@ -6,21 +7,55 @@ from pauxy.estimators.mixed import local_energy
 
 class ThermalWalker(object):
 
-    def __init__(self, weight, system, trial, stack_size=10):
+    def __init__(self, weight, system, trial, stack_size=2):
         self.weight = weight
         self.alive = True
         self.num_slices = trial.ntime_slices
+        print("# Number of slices = {}".format(self.num_slices))
         self.G = numpy.zeros(trial.dmat.shape, dtype=trial.dmat.dtype)
-        self.stack_length = self.num_slices // stack_size
-        # todo: Fix this hardcoded value
-        self.stack = PropagatorStack(stack_size, trial.ntime_slices,
+
+        if (stack_size == None):
+            print ("# Stack size is determined by BT")
+            emax = numpy.max(numpy.diag(trial.dmat[0]))
+            emin = numpy.min(numpy.diag(trial.dmat[0]))
+            self.stack_size = min(self.num_slices, 
+                int (1.5 / ((cmath.log(float(emax)) - cmath.log(float(emin))) / 8.0).real))
+            print ("# Initial stack size is {}".format(self.stack_size))
+        else:
+            self.stack_size = stack_size
+
+#       adjust stack size
+        lower_bound = self.stack_size
+        upper_bound = self.stack_size
+        while ((self.num_slices//lower_bound) * lower_bound < self.num_slices):
+            lower_bound -= 1
+        while ((self.num_slices//upper_bound) * upper_bound < self.num_slices):
+            upper_bound += 1
+
+        if ((self.stack_size-lower_bound) <= (upper_bound - self.stack_size)):
+            self.stack_size = lower_bound
+        else:
+            self.stack_size = upper_bound
+
+        print ("# upper_bound is {}".format(upper_bound))
+        print ("# lower_bound is {}".format(lower_bound))
+        print ("# Adjusted stack size is {}".format(self.stack_size))
+
+        self.stack_length = self.num_slices // self.stack_size
+
+        self.stack = PropagatorStack(self.stack_size, trial.ntime_slices,
                                      trial.dmat.shape[-1], trial.dmat.dtype)
+
         # Initialise all propagators to the trial density matrix.
         self.stack.set_all(trial.dmat)
         self.greens_function(trial)
         self.ot = 1.0
 
+
     def greens_function(self, trial, slice_ix = None):
+        # self.greens_function_svd(trial, slice_ix)
+        self.greens_function_qr(trial, slice_ix)
+    def greens_function_svd(self, trial, slice_ix = None):
         if (slice_ix == None):
             slice_ix = self.stack.time_slice
 
@@ -51,15 +86,62 @@ class ThermalWalker(object):
             #      = V3^{\dagger} D3 U3^{\dagger}
             self.G[spin] = (V3.conj().T).dot(D3).dot(U3.conj().T)
 
-    def construct_greens_function_unstable(self, slice_ix):
-        I = numpy.identity(self.G.shape[-1], dtype=self.G.dtype)
-        A = numpy.array([I,I])
-        for i in range(0, self.stack.nbins):
-            ix = (slice_ix + i) % self.stack.nbins
-            B = self.stack.get(ix)
-            A[0] = B[0].dot(A[0])
-            A[1] = B[1].dot(A[1])
-        self.G = greens_function(A)
+    def greens_function_qr(self, trial, slice_ix = None):
+        if (slice_ix == None):
+            slice_ix = self.stack.time_slice
+
+        bin_ix = slice_ix // self.stack.stack_width
+
+        for spin in [0, 1]:
+            # Need to construct the product A(l) = B_l B_{l-1}..B_L...B_{l+1}
+            # in stable way. Iteratively construct SVD decompositions starting
+            # from the rightmost (product of) propagator(s).
+            B = self.stack.get((bin_ix+1)%self.stack.nbins)
+            # (U1, S1, V1) = scipy.linalg.svd(B[spin])
+            (U1, V1) = numpy.linalg.qr(B[spin])
+            for i in range(2, self.stack.nbins+1):
+                ix = (bin_ix + i) % self.stack.nbins
+                B = self.stack.get(ix)
+                T1 = numpy.dot(B[spin], U1)
+                # (U1, V, P) = scipy.linalg.qr(T1, pivoting = True)
+                (U1, V) = scipy.linalg.qr(T1, pivoting = False)
+            
+                # Pmat = numpy.zeros((len(P),len(P)))
+                # for j in range (len(P)):
+                #     Pmat[P[j],j] = 1
+                # V = numpy.dot(V, Pmat.T)
+            
+                V1 = numpy.dot(V, V1)
+            
+            # Final SVD decomposition to construct G(l) = [I + A(l)]^{-1}.
+            # Care needs to be taken when adding the identity matrix.
+            V1inv = scipy.linalg.solve_triangular(V1, numpy.identity(V1.shape[0]))
+
+            T3 = numpy.dot(U1.conj().T, V1inv) + numpy.identity(V1.shape[0])
+            # (U2, V2, P) = scipy.linalg.qr(T3, pivoting = True)
+            (U2, V2) = scipy.linalg.qr(T3, pivoting = False)
+
+            # Pmat = numpy.zeros((len(P),len(P)))
+            # for j in range (len(P)):
+            #     Pmat[P[j],j] = 1
+            # V2 = numpy.dot(V2, Pmat.T)
+
+            U3 = numpy.dot(U1, U2)
+            V3 = numpy.dot(V2, V1)
+            V3inv = scipy.linalg.solve_triangular(V3, numpy.identity(V3.shape[0]))
+            # G(l) = (U3 S2 V3)^{-1}
+            #      = V3^{\dagger} D3 U3^{\dagger}
+            self.G[spin] = (V3inv).dot(U3.conj().T)
+
+    # def construct_greens_function_unstable(self, slice_ix):
+    #     I = numpy.identity(self.G.shape[-1], dtype=self.G.dtype)
+    #     A = numpy.array([I,I])
+    #     for i in range(0, self.stack.nbins):
+    #         ix = (slice_ix + i) % self.stack.nbins
+    #         B = self.stack.get(ix)
+    #         A[0] = B[0].dot(A[0])
+    #         A[1] = B[1].dot(A[1])
+    #     self.G = greens_function(A)
 
     def local_energy(self, system):
         rdm = one_rdm_from_G(self.G)
@@ -137,20 +219,33 @@ def unit_test():
     from pauxy.systems.ueg import UEG
     from pauxy.trial_density_matrices.onebody import OneBody
 
-    inputs = {'nup':1, 
-    'ndown':1,
-    'rs':1.0,
-    'ecut':1,
-    "name": "one_body",
-    "mu":1.94046021
-    }
-    beta = 2.0
-    dt = 0.05
-    system = UEG(inputs, True)
-    trial = OneBody(inputs, system, beta, dt, system.H1, verbose=True)
-    print(numpy.diag(trial.dmat[0]), numpy.diag(trial.dmat[1]))
-    walker = ThermalWalker(1.0, system, trial, stack_size=10)
+    # inputs = {'nup':1, 
+    # 'ndown':1,
+    # 'rs':1.0,
+    # 'ecut':0.5,
+    # "name": "one_body",
+    # "mu":1.94046021
+    # }
+    # beta = 2.0
+    # dt = 0.05
+    # system = UEG(inputs, True)
+    # trial = OneBody(inputs, system, beta, dt, system.H1, verbose=True)
+    # walker = ThermalWalker(1.0, system, trial, stack_size=None)
+    # (Q, R, P) = scipy.linalg.qr(walker.stack.get(0)[0], pivoting = True)
+    N = 100
 
+    A = numpy.random.rand(N,N)
+    Q, R, P = scipy.linalg.qr(A, pivoting = True)
+    Pmat = numpy.zeros((N,N))
+    for i in range (N):
+        Pmat[P[i],i] = 1
+
+    print(A - Q.dot(R).dot(Pmat.T))
+    # print (P)
+    # R = numpy.random.rand(system.nbasis, system.nbasis)
+    # R = numpy.triu(R)
+    # R = scipy.sparse.csc_matrix(R)
+    # print(R*R)
     # system.scaled_density_operator_incore(system.qvecs)
 
     # for (i, qi) in enumerate(system.qvecs):
