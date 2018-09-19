c4eb2457c56bfd880bb8e82fa46f1d8dd2cbbca4
Fionn Malone <f.malone13@imperial.ac.uk>
2018-09-19 13:43:00 -0700

    Refactor estimators.

---
+++ pauxy/estimators/generic.py
@@ -0,0 +1,79 @@
+import numpy
+
+def local_energy_generic(system, G, Ghalf=None):
+    r"""Calculate local for generic two-body hamiltonian.
+
+    This uses the full form for the two-electron integrals.
+
+    For testing purposes only.
+
+    Parameters
+    ----------
+    system : :class:`hubbard`
+        System information for the hubbard model.
+    G : :class:`numpy.ndarray`
+        Walker's "green's function"
+
+    Returns
+    -------
+    (E, T, V): tuple
+        Local, kinetic and potential energies.
+    """
+    e1 = (numpy.einsum('ij,ji->', system.T[0], G[0]) +
+          numpy.einsum('ij,ji->', system.T[1], G[1]))
+    euu = 0.5*(numpy.einsum('pqrs,pr,qs->', system.h2e, G[0], G[0]) -
+               numpy.einsum('pqrs,ps,qr->', system.h2e, G[0], G[0]))
+    edd = 0.5*(numpy.einsum('pqrs,pr,qs->', system.h2e, G[1], G[1]) -
+               numpy.einsum('pqrs,ps,qr->', system.h2e, G[1], G[1]))
+    eud = 0.5*numpy.einsum('pqrs,pr,qs->', system.h2e, G[0], G[1])
+    edu = 0.5*numpy.einsum('pqrs,pr,qs->', system.h2e, G[1], G[0])
+    e2 = euu + edd + eud + edu
+    return (e1+e2+system.ecore, e1+system.ecore, e2)
+
+def local_energy_generic_opt(system, G, Ghalf=None):
+    # Element wise multiplication.
+    e1b = numpy.sum(system.T[0]*G[0]) + numpy.sum(system.T[1]*G[1])
+    Gup = Ghalf[0].ravel()
+    Gdn = Ghalf[1].ravel()
+    euu = 0.5 * Gup.dot(system.vaklb[0].dot(Gup))
+    edd = 0.5 * Gdn.dot(system.vaklb[1].dot(Gdn))
+    eud = 0.5 * numpy.dot(Gup.T*system.rchol_vecs[0],
+                          Gdn.T*system.rchol_vecs[1])
+    edu = 0.5 * numpy.dot(Gdn.T*system.rchol_vecs[1],
+                          Gup.T*system.rchol_vecs[0])
+    e2b = euu + edd + eud + edu
+    return (e1b + e2b + system.ecore, e1b + system.ecore, e2b)
+
+def local_energy_generic_cholesky(system, G, Ghalf=None):
+    r"""Calculate local for generic two-body hamiltonian.
+
+    This uses the cholesky decomposed two-electron integrals.
+
+    Parameters
+    ----------
+    system : :class:`hubbard`
+        System information for the hubbard model.
+    G : :class:`numpy.ndarray`
+        Walker's "green's function"
+
+    Returns
+    -------
+    (E, T, V): tuple
+        Local, kinetic and potential energies.
+    """
+    e1 = (numpy.einsum('ij,ji->', system.T[0], G[0]) +
+          numpy.einsum('ij,ji->', system.T[1], G[1]))
+    euu = 0.5*(numpy.einsum('lpr,lqs,pr,qs->', system.chol_vecs,
+                            system.chol_vecs, G[0], G[0]) -
+               numpy.einsum('lpr,lqs,ps,qr->', system.chol_vecs,
+                            system.chol_vecs, G[0], G[0]))
+    edd = 0.5*(numpy.einsum('lpr,lqs,pr,qs->', system.chol_vecs,
+                            system.chol_vecs, G[1], G[1]) -
+               numpy.einsum('lpr,lqs,ps,qr->', system.chol_vecs,
+                            system.chol_vecs, G[1], G[1]))
+    eud = 0.5*numpy.einsum('lpr,lqs,pr,qs->', system.chol_vecs,
+                           system.chol_vecs, G[0], G[1])
+    edu = 0.5*numpy.einsum('lpr,lqs,pr,qs->', system.chol_vecs,
+                           system.chol_vecs, G[1], G[0])
+    e2 = euu + edd + eud + edu
+    return (e1+e2+system.ecore, e1+system.ecore, e2)
---
+++ pauxy/estimators/hubbard.py
@@ -0,0 +1,86 @@
+import numpy
+
+def local_energy_hubbard(system, G, Ghalf=None):
+    r"""Calculate local energy of walker for the Hubbard model.
+
+    Parameters
+    ----------
+    system : :class:`Hubbard`
+        System information for the Hubbard model.
+    G : :class:`numpy.ndarray`
+        Walker's "Green's function"
+
+    Returns
+    -------
+    (E_L(phi), T, V): tuple
+        Local, kinetic and potential energies of given walker phi.
+    """
+    ke = numpy.sum(system.T[0] * G[0] + system.T[1] * G[1])
+    # Todo: Stupid
+    pe = sum(system.U * G[0][i][i] * G[1][i][i]
+             for i in range(0, system.nbasis))
+
+    return (ke + pe, ke, pe)
+
+
+def local_energy_hubbard_ghf(system, Gi, weights, denom):
+    """Calculate local energy of GHF walker for the Hubbard model.
+
+    Parameters
+    ----------
+    system : :class:`Hubbard`
+        System information for the Hubbard model.
+    Gi : :class:`numpy.ndarray`
+        Array of Walker's "Green's function"
+    denom : float
+        Overlap of trial wavefunction with walker.
+
+    Returns
+    -------
+    (E_L(phi), T, V): tuple
+        Local, kinetic and potential energies of given walker phi.
+    """
+    ke = numpy.einsum('i,ikl,kl->', weights, Gi, system.Text) / denom
+    # numpy.diagonal returns a view so there should be no overhead in creating
+    # temporary arrays.
+    guu = numpy.diagonal(Gi[:,:system.nbasis,:system.nbasis], axis1=1, axis2=2)
+    gdd = numpy.diagonal(Gi[:,system.nbasis:,system.nbasis:], axis1=1, axis2=2)
+    gud = numpy.diagonal(Gi[:,system.nbasis:,:system.nbasis], axis1=1, axis2=2)
+    gdu = numpy.diagonal(Gi[:,:system.nbasis,system.nbasis:], axis1=1, axis2=2)
+    gdiag = guu*gdd - gud*gdu
+    pe = system.U * numpy.einsum('j,jk->', weights, gdiag) / denom
+    return (ke+pe, ke, pe)
+
+def local_energy_hubbard_ghf_full(system, GAB, weights):
+    r"""Calculate local energy of GHF walker for the Hubbard model.
+
+    Parameters
+    ----------
+    system : :class:`Hubbard`
+        System information for the Hubbard model.
+    GAB : :class:`numpy.ndarray`
+        Matrix of Green's functions for different SDs A and B.
+    weights : :class:`numpy.ndarray`
+        Components of overlap of trial wavefunction with walker.
+
+    Returns
+    -------
+    (E_L, T, V): tuple
+        Local, kinetic and potential energies of given walker phi.
+    """
+    denom = numpy.sum(weights)
+    ke = numpy.einsum('ij,ijkl,kl->', weights, GAB, system.Text) / denom
+    # numpy.diagonal returns a view so there should be no overhead in creating
+    # temporary arrays.
+    guu = numpy.diagonal(GAB[:,:,:system.nbasis,:system.nbasis], axis1=2,
+                         axis2=3)
+    gdd = numpy.diagonal(GAB[:,:,system.nbasis:,system.nbasis:], axis1=2,
+                         axis2=3)
+    gud = numpy.diagonal(GAB[:,:,system.nbasis:,:system.nbasis], axis1=2,
+                         axis2=3)
+    gdu = numpy.diagonal(GAB[:,:,:system.nbasis,system.nbasis:], axis1=2,
+                         axis2=3)
+    gdiag = guu*gdd - gud*gdu
+    pe = system.U * numpy.einsum('ij,ijk->', weights, gdiag) / denom
+    return (ke+pe, ke, pe)
+
