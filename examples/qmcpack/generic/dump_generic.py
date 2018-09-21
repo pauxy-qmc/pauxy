498232cdd57473e85a6c97d64913b583309a8b3d
Fionn Malone <f.malone13@imperial.ac.uk>
2018-09-21 16:47:31 -0700

    Fix generic dump.

--- examples/qmcpack/generic/dump_generic.py
+++ examples/qmcpack/generic/dump_generic.py
@@ -2,13 +2,15 @@
 # Dump Cholesky factorisation and trial wavefunction to file for QMCPACK.
 import numpy
 from pauxy.systems.generic import Generic
+from pauxy.trial_wavefunction.hartree_fock import HartreeFock
 from pauxy.utils.io import dump_qmcpack_cholesky, dump_qmcpack_trial_wfn
 
 options = {'nup': 5, 'ndown': 5, 'integrals': 'fcidump.h5'}
 
 atom = Generic(options, verbose=True)
+trial = HartreeFock(atom, True, {})
 
 # Factor of 4 is down to conventions in factorisations.
-dump_qmcpack_cholesky(atom.h1e_mod, atom.schol_vecs, (atom.nup, atom.ndown),
+dump_qmcpack_cholesky(atom.T, atom.schol_vecs, (atom.nup, atom.ndown),
                       atom.nbasis, atom.ecore, filename='neon.hamiltonian.h5')
-dump_qmcpack_trial_wfn(numpy.eye(atom.nbasis), atom.ne, filename='neon.wfn.dat')
+dump_qmcpack_trial_wfn(atom.mo_coeff, atom.ne, filename='neon.wfn.dat')
