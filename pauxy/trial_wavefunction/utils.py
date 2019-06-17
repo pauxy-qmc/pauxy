3528d8ad66d05ffbc382f3c5a0db98b7b1ad6b50
Fionn Malone <f.malone13@imperial.ac.uk>
2019-06-16 21:04:45 -0700

    Use multi slater for default hartree fock option.

--- pauxy/trial_wavefunction/utils.py
+++ pauxy/trial_wavefunction/utils.py
@@ -1,3 +1,4 @@
+import numpy
 from pauxy.trial_wavefunction.free_electron import FreeElectron
 from pauxy.trial_wavefunction.uhf  import UHF
 from pauxy.trial_wavefunction.hartree_fock import HartreeFock
@@ -26,12 +27,24 @@ def get_trial_wavefunction(system, options={}, mf=None, parallel=False, verbose=
     """
     if mf is not None:
         wfn, coeffs = get_pyscf_wfn(system, mf)
-        trial = MultiSlater(system, wfn, coeffs)
+        trial = MultiSlater(system, wfn, coeffs,
+                            parallel=parallel, verbose=verbose)
+    elif options['name'] == 'hartree_fock':
+        na = system.nup
+        nb = system.ndown
+        wfn = numpy.zeros((1,system.nbasis,system.nup+system.ndown),
+                          dtype=numpy.complex128)
+        coeffs = numpy.array([1.0+0j])
+        I = numpy.identity(system.nbasis, dtype=numpy.complex128)
+        wfn[0,:,:na] = I[:,:na]
+        wfn[0,:,na:] = I[:,:nb]
+        trial = MultiSlater(system, wfn, coeffs, options=options,
+                            parallel=parallel, verbose=verbose)
     elif options['name'] == 'free_electron':
         trial = FreeElectron(system, True, options, parallel, verbose)
     elif options['name'] == 'UHF':
         trial = UHF(system, True, options, parallel, verbose)
-    elif options['name'] == 'multi_slater':
-        trial = HartreeFock(system, True, options, parallel, verbose)
+    else:
+        trial = None
 
     return trial
