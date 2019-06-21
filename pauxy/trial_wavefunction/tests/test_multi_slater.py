45aae48b819fa9896ec61b7964ef4a638f3df221
Fionn Malone <fionn.malone@gmail.com>
2019-06-21 15:12:10 -0700

    Update unit test.

--- pauxy/trial_wavefunction/tests/test_multi_slater.py
+++ pauxy/trial_wavefunction/tests/test_multi_slater.py
@@ -1,8 +1,11 @@
 import numpy
 import scipy.linalg
 import unittest
-from pyscf import gto, ao2mo, scf, fci
-from pauxy.estimators.mixed import variational_energy_multi_det
+import sys
+from pyscf import gto, ao2mo, scf, fci, tools
+from pauxy.estimators.mixed import variational_energy_multi_det, local_energy
+from pauxy.estimators.greens_function import gab
+from pauxy.estimators.misc import get_hmatel
 from pauxy.systems.generic import Generic
 from pauxy.systems.ueg import UEG
 from pauxy.utils.from_pyscf import integrals_from_scf
@@ -43,50 +46,39 @@ class TestMultiSlater(unittest.TestCase):
         self.assertAlmostEqual(e[0],0.15400990069739182)
         self.assertAlmostEqual(e[0],evar[0])
 
-    def test_ci(self):
-        mol = gto.M(atom=[('Be', 0, 0, 0)], basis='sto-3g', verbose=0)
+    # Todo: move to estimator tests.
+    def test_slater_condon(self):
+        mol = gto.M(atom=[('C', 0, 0, 0)], basis='sto-3g', verbose=0)
         mf = scf.RHF(mol)
         ehf = mf.kernel()
-        # h1e, chol, ecore, oao = integrals_from_scf(mf, verbose=0, chol_cut=1e-5)
-        # nb = h1e.shape[0]
-        h1 = mf.mo_coeff.T.dot(mf.get_hcore()).dot(mf.mo_coeff)
-        eri = ao2mo.kernel(mol, mf.mo_coeff)
+        h1e, chol, ecore, oao = integrals_from_scf(mf, verbose=0, chol_cut=1e-5,
+                                                   ortho_ao=False)
+        nb = h1e.shape[0]
+        system = Generic(nelec=mf.mol.nelec, h1e=h1e,
+                         chol=chol.reshape((-1,nb,nb)),
+                         ecore=ecore, verbose=0)
+        eri = ao2mo.kernel(mol, mf.mo_coeff, aosym=1)
+        tools.fcidump.from_scf(mf, 'fcidump')
+        system.oao = mf.mo_coeff
         cisolver = fci.direct_spin1.FCI(mol)
-        e, ci = cisolver.kernel(h1, eri, h1.shape[1], mol.nelec,
-                                ecore=mol.energy_nuc())
-        coeff, oa, ob = zip(*fci.addons.large_ci(ci, mf.mo_coeff.shape[0], mol.nelec,
-                                   tol=0, return_strs=False))
-        oa = [o.tolist() for o in oa]
-        ob = [o.tolist() for o in ob]
-        ndets = len(oa)
+        # H_fci = fci.direct_spin1.pspace(h1e, eri, nb, mol.nelec)[1]
+        # e_all, v_all = numpy.linalg.eigh(H_fci)
+
+        e_fci, ci_fci = cisolver.kernel(h1e, eri, h1e.shape[1], mol.nelec,
+                                        ecore=mol.energy_nuc())
+        coeff, oa, ob = zip(*fci.addons.large_ci(ci_fci, mf.mo_coeff.shape[0],
+                                                 mol.nelec, tol=0,
+                                                 return_strs=False))
+        # Unpack determinants into spin orbital basis.
+        oa = [[2*x for x in o.tolist()] for o in oa]
+        oa = [a+[2*x+1 for x in o.tolist()] for (a,o) in zip(oa,ob)]
+        dets = [numpy.sort(numpy.array(x)) for x in oa]
+        ndets = len(dets)
+
         H = numpy.zeros((ndets,ndets))
-        print(eri.shape)
-        class Excit:
-            def __init__(self):
-                i = None
-                j = None
-                a = None
-                b = None
-                sign = 1.0
-
-        def get_excit(di, dj):
-            ex = Excit()
-            na = len(di[0])
-            nb = len(di[1])
-            nelec = (na,nb)
-            exa = set(di[0])-set(dj[0])
-            exb = set(di[1])-set(dj[1])
-            nexa = len(exa)
-            nexb = len(exb)
-            return ex
-
-        def slater_condon0(system, occs):
-            pass
-        def slater_condon1(system, occs):
-            pass
-        def slater_condon2(system, occs):
-            pass
-
         for i in range(ndets):
-            for j in range(ndets):
-                excit = get_excit((oa[i],ob[i]), (oa[j],ob[j]))
+            for j in range(i,ndets):
+                hmatel = get_hmatel(system, dets[i],dets[j])
+                H[i,j] = hmatel
+        e_direct, ev_direct = scipy.linalg.eigh(H,lower=False)
+        self.assertAlmostEqual(e_direct[0], e_fci)
