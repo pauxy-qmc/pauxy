e7be5fe2c3a2f667e37793527d33c9b0b40f6736
Fionn Malone <fionn.malone@gmail.com>
2019-07-12 16:11:45 -0700

    Fix ordering of occupied orbitals in determinant.
    To be consistent with green's function evaluation need alpha electrons
    stored first followed by beta.

--- pauxy/estimators/ci.py
+++ pauxy/estimators/ci.py
@@ -2,15 +2,17 @@ import numpy
 import scipy.linalg
 import itertools
 
-def simple_fci(system, gen_dets=False):
+def simple_fci(system, gen_dets=False, occs=None, hamil=True):
     """Very dumb FCI routine."""
     orbs = numpy.arange(system.nbasis)
-    oa = [c for c in itertools.combinations(orbs, system.nup)]
-    ob = [c for c in itertools.combinations(orbs, system.ndown)]
-    oa, ob = zip(*itertools.product(oa,ob))
+    if occs is None:
+        oa = [c for c in itertools.combinations(orbs, system.nup)]
+        ob = [c for c in itertools.combinations(orbs, system.ndown)]
+        oa, ob = zip(*itertools.product(oa,ob))
+    else:
+        oa, ob = occs
     # convert to spin orbitals
-    dets = [[2*i for i in c] for c in oa]
-    dets = [alp+[2*i+1 for i in c] for (alp,c) in zip(dets,ob)]
+    dets = [[j for j in a] + [i+system.nbasis for i in c] for (a,c) in zip(oa,ob)]
     dets = [numpy.sort(d) for d in dets]
     ndets = len(dets)
     H = numpy.zeros((ndets,ndets))
@@ -19,6 +21,8 @@ def simple_fci(system, gen_dets=False):
             H[i,j] = get_hmatel(system, dets[i], dets[j])
     if gen_dets:
         return scipy.linalg.eigh(H, lower=False), (dets,numpy.array(oa),numpy.array(ob))
+    elif hamil:
+        return scipy.linalg.eigh(H, lower=False), H
     else:
         return scipy.linalg.eigh(H, lower=False)
 
@@ -33,16 +37,15 @@ def get_hmatel(system, di, dj):
     if nex == 0:
         hmatel = slater_condon0(system, di)[0]
     elif nex == 1:
-        i, si = map_orb(from_orb[0])
-        a, sa = map_orb(to_orb[0])
-        assert si == sa
+        i, si = map_orb(from_orb[0], system.nbasis)
+        a, sa = map_orb(to_orb[0], system.nbasis)
         hmatel = slater_condon1(system, (i,si), (a,sa), di, perm)
     elif nex == 2:
         # < ij | ab > or < ij | ba >
-        i, si = map_orb(from_orb[0])
-        j, sj = map_orb(from_orb[1])
-        a, sa = map_orb(to_orb[0])
-        b, sb = map_orb(to_orb[1])
+        i, si = map_orb(from_orb[0], system.nbasis)
+        j, sj = map_orb(from_orb[1], system.nbasis)
+        a, sa = map_orb(to_orb[0], system.nbasis)
+        b, sb = map_orb(to_orb[1], system.nbasis)
         hmatel = slater_condon2(system, (i,si), (j,sj), (a,sa), (b,sb), perm)
     else:
         hmatel = 0.0
@@ -57,12 +60,12 @@ def get_perm(from_orb, to_orb, di, dj):
     perm = 0
     for o in from_orb:
         io = numpy.where(dj==o)[0]
-        perm += len(dj) - io - 1 + nmove
+        perm += io - nmove
         nmove += 1
     nmove = 0
     for o in to_orb:
         io = numpy.where(di==o)[0]
-        perm += len(di) - io - 1 + nmove
+        perm += io - nmove
         nmove += 1
     return perm % 2 == 1
 
@@ -71,11 +74,11 @@ def slater_condon0(system, occs):
     e2b = 0.0
     e1b = system.ecore
     for i in range(len(occs)):
-        ii, spin_ii = map_orb(occs[i])
+        ii, spin_ii = map_orb(occs[i], system.nbasis)
         # Todo: Update if H1 is ever spin dependent.
         e1b += system.H1[0,ii,ii]
         for j in range(i+1,len(occs)):
-            jj, spin_jj = map_orb(occs[j])
+            jj, spin_jj = map_orb(occs[j], system.nbasis)
             e2b += system.hijkl(ii,jj,ii,jj)
             if spin_ii == spin_jj:
                 e2b -= system.hijkl(ii,jj,jj,ii)
@@ -90,7 +93,7 @@ def slater_condon1(system, i, a, occs, perm):
     for j in range(nel):
         # \sum_j <ij|aj> - <ij|ja>
         oj = occs[j]
-        oj, soj = map_orb(oj)
+        oj, soj = map_orb(oj, system.nbasis)
         if 2*oj+soj != 2*ii+si:
             hmatel += system.hijkl(ii,oj,aa,oj)
             if soj == si:
@@ -115,14 +118,14 @@ def slater_condon2(system, i, j, a, b, perm):
     else:
         return hmatel
 
-def map_orb(orb):
+def map_orb(orb, nbasis):
     """Map spin orbital to spatial index."""
-    if orb % 2 == 0:
+    if orb // nbasis == 0:
         s = 0
-        ix = orb // 2
+        ix = orb
     else:
         s = 1
-        ix = (orb-1) // 2
+        ix = orb - nbasis
     return ix, s
 
 def get_one_body_matel(ints, di, dj):
