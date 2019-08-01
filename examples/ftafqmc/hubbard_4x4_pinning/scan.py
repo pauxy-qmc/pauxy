68853887e9c65b1fe99364097ec111b7a044cd31
Fionn Malone <fionn.malone@gmail.com>
2019-08-01 15:44:12 -0700

    Combine scripts.

--- examples/ftafqmc/hubbard_4x4_pinning/scan.py
+++ examples/ftafqmc/hubbard_4x4_pinning/scan.py
@@ -34,6 +34,33 @@ for i, b in enumerate([2, 5, 10, 20]):
     for m in mu:
         system['mu'] = m
         afqmc = ThermalAFQMC(comm, system, qmc, estimates=estim,
-                             walker_opts=walker_opts,
                              verbose=(True and comm.rank==0))
         afqmc.run(comm=comm, verbose=True)
+
+# Rerun with optimised chemical potential values.
+# These are values from reference.
+# mus = numpy.array([0.44086182875227903,0.4611368640132099,
+                   # 0.4769148405517905,0.4863196188379373])
+# mus = 2.0 * mus
+for i, b in enumerate(beta):
+    files = glob.glob('estimates_beta_'+str(i)+'*.h5')
+    if comm.rank == 0:
+        data = analyse_energy(files)
+        vol = get_sys_param(files[0], 'vol')
+        mu = find_chem_pot(data, 0.875, vol)
+        print(b, mu, vol)
+    else:
+        mu = None
+    mu = comm.bcast(mu, root=0)
+    system["mu"] = mu
+    qmc["beta"] = b
+    estim = {
+        "overwrite": False,
+        "basename": "final_av_{}".format(i),
+        "mixed": {
+            "average_gf": True
+        }
+    }
+    afqmc = ThermalAFQMC(comm, system, qmc, estimates=estim,
+                         verbose=(True and comm.rank==0))
+    afqmc.run(comm=comm, verbose=True)
