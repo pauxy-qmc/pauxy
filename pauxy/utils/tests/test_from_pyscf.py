12883a1e5da65463b8cc8bd525593f78b030f01a
Fionn Malone <f.malone13@imperial.ac.uk>
2019-10-02 23:16:33 -0700

    Fix from pyscf test.

--- pauxy/utils/tests/test_from_pyscf.py
+++ pauxy/utils/tests/test_from_pyscf.py
@@ -1,39 +1,32 @@
 import os
-import unittest
-try:
-    from pyscf import gto, ao2mo, scf
-    from pauxy.utils.from_pyscf import integrals_from_scf, integrals_from_chkfile
-    no_pyscf = False
-except ImportError:
-    no_pyscf = True
+import pytest
+from pyscf import gto, ao2mo, scf
+from pauxy.utils.from_pyscf import (
+        integrals_from_scf,
+        integrals_from_chkfile,
+        get_pyscf_wfn
+        )
 
-class TestGeneric(unittest.TestCase):
+def test_from_pyscf():
+    atom = gto.M(atom='Ne 0 0 0', basis='sto-3g', verbose=0)
+    mf = scf.RHF(atom)
+    mf.kernel()
+    h1e, chol, ecore, oao = integrals_from_scf(mf, verbose=0, chol_cut=1e-5)
+    nb = h1e.shape[0]
 
-    @unittest.skipIF(no_pyscf, "PYSCF not found.")
-    def test_from_pyscf(self):
-        atom = gto.M(atom='Ne 0 0 0', basis='sto-3g', verbose=0)
-        mf = scf.RHF(atom)
-        mf.kernel()
-        h1e, chol, ecore, oao = integrals_from_scf(mf, verbose=0, chol_cut=1e-5)
-        nb = h1e.shape[0]
+def test_from_chkfile():
+    atom = gto.M(atom='Ne 0 0 0', basis='sto-3g', verbose=0)
+    mf = scf.RHF(atom)
+    mf.chkfile = 'scf.chk'
+    mf.kernel()
+    h1e, chol, ecore, oao, mol = integrals_from_chkfile('scf.chk', verbose=0, chol_cut=1e-5)
+    nb = h1e.shape[0]
 
-    @unittest.skipIF(no_pyscf, "PYSCF not found.")
-    def test_from_chkfile(self):
-        atom = gto.M(atom='Ne 0 0 0', basis='sto-3g', verbose=0)
-        mf = scf.RHF(atom)
-        mf.chkfile = 'scf.chk'
-        mf.kernel()
-        h1e, chol, ecore, oao, mol = integrals_from_chkfile('scf.chk', verbose=0, chol_cut=1e-5)
-        nb = h1e.shape[0]
-
-    def tearDown(self):
-        cwd = os.getcwd()
-        files = ['scf.chk']
-        for f in files:
-            try:
-                os.remove(cwd+'/'+f)
-            except OSError:
-                pass
-
-if __name__ == '__main__':
-    unittest.main()
+def tearDown(self):
+    cwd = os.getcwd()
+    files = ['scf.chk']
+    for f in files:
+        try:
+            os.remove(cwd+'/'+f)
+        except OSError:
+            pass
