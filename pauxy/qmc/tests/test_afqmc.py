897a5e6cc41ef8a3a01efc706d69762b9ef5a00b
Fionn Malone <f.malone13@imperial.ac.uk>
2019-10-02 22:12:54 -0700

    Update driver test.

--- pauxy/qmc/tests/test_afqmc.py
+++ pauxy/qmc/tests/test_afqmc.py
@@ -1,4 +1,4 @@
-import unittest
+import pytest
 from mpi4py import MPI
 import os
 from pyscf import gto, ao2mo, scf
@@ -9,96 +9,65 @@ from pauxy.systems.ueg import UEG
 from pauxy.trial_wavefunction.hartree_fock import HartreeFock
 from pauxy.utils.from_pyscf import integrals_from_scf
 
-class TestDriver(unittest.TestCase):
-
-    def test_from_pyscf(self):
-        atom = gto.M(atom='Ne 0 0 0', basis='cc-pvdz', verbose=0)
-        mf = scf.RHF(atom)
-        ehf = mf.kernel()
-        options = {
-                'get_sha1': False,
-                'qmc': {
-                        'timestep': 0.01,
-                        'num_steps': 10,
-                        'num_blocks': 10,
-                        'rng_seed': 8,
-                    },
-                }
-        comm = MPI.COMM_WORLD
-        afqmc = AFQMC(comm=comm, options=options, mf=mf, verbose=0)
-        afqmc.run(comm=comm, verbose=0)
-        afqmc.finalise(verbose=0)
-
-    def test_ueg(self):
-        options = {
-                'verbosity': 0,
-                'get_sha1': False,
-                'qmc': {
-                    'timestep': 0.01,
-                    'num_steps': 10,
-                    'num_blocks': 10,
-                    'rng_seed': 8,
-                },
-                'model': {
-                    'name': "UEG",
-                    'rs': 2.44,
-                    'ecut': 4,
-                    'nup': 7,
-                    'ndown': 7,
-                },
-                'trial': {
-                    'name': 'hartree_fock'
-                }
+def test_ueg():
+    options = {
+            'verbosity': 0,
+            'get_sha1': False,
+            'qmc': {
+                'timestep': 0.01,
+                'num_steps': 10,
+                'num_blocks': 10,
+                'rng_seed': 8,
+            },
+            'model': {
+                'name': "UEG",
+                'rs': 2.44,
+                'ecut': 4,
+                'nup': 7,
+                'ndown': 7,
+            },
+            'trial': {
+                'name': 'hartree_fock'
             }
-        comm = MPI.COMM_WORLD
-        # FDM: Fix cython issue.
+        }
+    comm = MPI.COMM_WORLD
+    # FDM: Fix cython issue.
+    afqmc = AFQMC(comm=comm, options=options)
+    afqmc.run(comm=comm, verbose=0)
+    afqmc.finalise(verbose=0)
+    ehy = afqmc.psi.walkers[0].hybrid_energy
+    assert ehy.real == pytest.approx(1.1153859035083666)
+    assert ehy.imag == pytest.approx(0.17265962035671692)
+
+def test_constructor():
+    options = {
+            'verbosity': 0,
+            'get_sha1': False,
+            'qmc': {
+                'timestep': 0.01,
+                'num_steps': 10,
+                'num_blocks': 10,
+                'rng_seed': 8,
+            },
+        }
+    model = {
+        'name': "UEG",
+        'rs': 2.44,
+        'ecut': 4,
+        'nup': 7,
+        'ndown': 7,
+        }
+    system = UEG(model)
+    trial = HartreeFock(system, True, {})
+    comm = MPI.COMM_WORLD
+    afqmc = AFQMC(comm=comm, options=options, system=system, trial=trial)
+    assert afqmc.trial.energy.real == pytest.approx(1.7796083856572522)
+
+def teardown_module(self):
+    cwd = os.getcwd()
+    files = ['estimates.0.h5']
+    for f in files:
         try:
-            afqmc = AFQMC(comm=comm, options=options)
-            afqmc.run(comm=comm, verbose=0)
-            afqmc.finalise(verbose=0)
-            # ref = 6.828957055614434+0.22576828445100017j
-            # ref = 6.821009376769289+0.13276828693227866j
-            # FDM: Update reference following merge sort update.
-            # FDM: Check reason for failure.
-            # ref = 6.562928368348016+0.07235261291158207j
-            cur = afqmc.psi.walkers[0].phi.trace()
-            # self.assertAlmostEqual(cur.real, ref.real)
-            # self.assertAlmostEqual(cur.imag, ref.imag)
-        except NameError:
+            os.remove(cwd+'/'+f)
+        except OSError:
             pass
-
-    def test_constructor(self):
-        options = {
-                'verbosity': 0,
-                'get_sha1': False,
-                'qmc': {
-                    'timestep': 0.01,
-                    'num_steps': 10,
-                    'num_blocks': 10,
-                    'rng_seed': 8,
-                },
-            }
-        model = {
-            'name': "UEG",
-            'rs': 2.44,
-            'ecut': 4,
-            'nup': 7,
-            'ndown': 7,
-            }
-        system = UEG(model)
-        trial = HartreeFock(system, True, {})
-        comm = MPI.COMM_WORLD
-        afqmc = AFQMC(comm=comm, options=options, system=system, trial=trial)
-        self.assertAlmostEqual(afqmc.trial.energy.real, 1.7796083856572522)
-
-    def tearDown(self):
-        cwd = os.getcwd()
-        files = ['estimates.0.h5']
-        for f in files:
-            try:
-                os.remove(cwd+'/'+f)
-            except OSError:
-                pass
-
-if __name__ == '__main__':
-    unittest.main()
