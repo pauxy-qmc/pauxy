1b2a296bcd08653c1f075049417a377409c540cd
Fionn Malone <f.malone13@imperial.ac.uk>
2019-10-02 21:31:19 -0700

    Update CI test.

--- pauxy/estimators/tests/test_ci.py
+++ pauxy/estimators/tests/test_ci.py
@@ -1,19 +1,13 @@
-import unittest
-import numpy
+import pytest
 from pauxy.systems.ueg import UEG
 from pauxy.estimators.ci import simple_fci
 
-class TestCI(unittest.TestCase):
-
-    def test_ueg(self):
-        sys = UEG({'rs': 2, 'nup': 2, 'ndown': 2, 'ecut': 0.5})
-        sys.ecore = 0
-        eig, evec = simple_fci(sys)
-        self.assertEqual(len(eig),441)
-        self.assertAlmostEqual(eig[0], 1.327088181107)
-        self.assertAlmostEqual(eig[231], 2.883365264420)
-        self.assertAlmostEqual(eig[424], 3.039496944900)
-        self.assertAlmostEqual(eig[-1],  3.207573492596)
-
-if __name__ == '__main__':
-    unittest.main()
+def test_ueg():
+    sys = UEG({'rs': 2, 'nup': 2, 'ndown': 2, 'ecut': 0.5})
+    sys.ecore = 0
+    eig, evec = simple_fci(sys)
+    assert len(eig) == 441
+    assert eig[0] == pytest.approx(1.327088181107)
+    assert eig[231] == pytest.approx(2.883365264420)
+    assert eig[424] == pytest.approx(3.039496944900)
+    assert eig[-1] == pytest.approx(3.207573492596)
