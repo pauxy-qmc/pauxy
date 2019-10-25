e56c67c31b077630a3297f4d09818aedd3a2444a
Fionn Malone <fionn.malone@gmail.com>
2019-10-25 10:54:11 -0700

    Update mean field density matrix.

--- pauxy/trial_density_matrices/mean_field.py
+++ pauxy/trial_density_matrices/mean_field.py
@@ -4,6 +4,7 @@ from pauxy.estimators.thermal import (
         one_rdm_stable, particle_number, entropy, greens_function
         )
 from pauxy.estimators.mixed import local_energy
+from pauxy.estimators.fock import fock_matrix
 from pauxy.trial_density_matrices.onebody import OneBody
 from pauxy.trial_density_matrices.chem_pot import (
         find_chemical_potential,
@@ -16,6 +17,8 @@ class MeanField(OneBody):
         OneBody.__init__(self, comm, system, beta, dt,
                          options, H1=H1, verbose=verbose)
         self.alpha = options.get('alpha', 0.75)
+        self.max_scf_it = options.get('max_scf_it', self.max_it)
+        self.max_macro_it = options.get('max_macro_it', self.max_it)
         if comm.rank == 0:
             P, HMF, mu = self.thermal_hartree_fock(system, beta)
             muN = mu * numpy.eye(system.nbasis, dtype=self.G.dtype)
@@ -41,7 +44,7 @@ class MeanField(OneBody):
         P = self.P.copy()
         if self.verbose:
             print("# Determining Thermal Hartree--Fock Density Matrix.")
-        for it in range(self.max_it):
+        for it in range(self.max_macro_it):
             if self.verbose:
                 print("# Macro iteration: {}".format(it))
             HMF = self.scf(system, beta, mu_old, P)
@@ -64,7 +67,7 @@ class MeanField(OneBody):
 
     def scf(self, system, beta, mu, P):
         # 1. Compute HMF
-        HMF, cfac = compute_HMF(system, P)
+        HMF = fock_matrix(system, P)
         dt = self.dtau
         muN = mu * numpy.eye(system.nbasis, dtype=self.G.dtype)
         rho = numpy.array([scipy.linalg.expm(-dt*(HMF[0]-muN)),
@@ -72,8 +75,8 @@ class MeanField(OneBody):
         Pold = one_rdm_stable(rho, self.num_bins)
         if self.verbose:
             print(" # Running Thermal SCF.")
-        for it in range(self.max_it):
-            HMF, cfac = compute_HMF(system, Pold)
+        for it in range(self.max_scf_it):
+            HMF = fock_matrix(system, Pold)
             rho = numpy.array([scipy.linalg.expm(-dt*(HMF[0]-muN)),
                                scipy.linalg.expm(-dt*(HMF[1]-muN))])
             Pnew = (1-self.alpha)*one_rdm_stable(rho, self.num_bins) + self.alpha*Pold
@@ -92,13 +95,3 @@ class MeanField(OneBody):
             N = particle_number(P).real
             print(" # Average particle number: {:13.8e}".format(N))
         return HMF
-
-def compute_HMF(system, P):
-    if system.sparse:
-        mf_shift = 1j*P[0].ravel()*system.hs_pot
-        mf_shift += 1j*P[1].ravel()*system.hs_pot
-        VMF = 1j*system.hs_pot.dot(mf_shift).reshape(system.nbasis,system.nbasis)
-    else:
-        mf_shift = 1j*numpy.einsum('lpq,spq->l', system.hs_pot, P)
-        VMF = 1j*numpy.einsum('lpq,l->pq', system.hs_pot, mf_shift)
-    return system.h1e_mod - VMF, 0.5*numpy.dot(mf_shift,mf_shift)
