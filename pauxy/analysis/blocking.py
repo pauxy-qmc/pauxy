127411f053ff6ee4bf8097de0d3f7246415be644
Fionn Malone <fionn.malone@gmail.com>
2019-11-23 21:31:00 -0800

    Fix blocking script.

--- pauxy/analysis/blocking.py
+++ pauxy/analysis/blocking.py
@@ -264,14 +264,14 @@ def analyse_estimates(files, start_time, multi_sim=False):
     for f in files:
         md = get_metadata(f)
         read_rs = get_from_dict(md, ['psi', 'read_rs'])
-        step = get_from_dict(md, ['qmc', 'nmeasure'])
+        step = get_from_dict(md, ['qmc', 'nsteps'])
         dt = get_from_dict(md, ['qmc', 'dt'])
         start = int(start_time/(step*dt)) + 1
         if read_rs:
             start = 0
         data = extract_mixed_estimates(f, start)
         columns = set_info(data, md)
-        basic.append(data)
+        basic.append(data.drop('Iteration', axis=1))
         mds.append(md)
     basic = pd.concat(basic).groupby(columns)
     basic_av = reblock_mixed(basic, columns)
@@ -281,5 +281,5 @@ def analyse_estimates(files, start_time, multi_sim=False):
     print(basic_av.to_string(index=False, float_format=fmt))
     with h5py.File(outfile, 'w') as fh5:
         fh5['metadata'] = numpy.array(mds).astype('S')
-        fh5['basic/estimates'] = basic_av.values.astype(float)
+        fh5['basic/estimates'] = basic_av.drop('integrals',axis=1).values.astype(float)
         fh5['basic/headers'] = numpy.array(basic_av.columns.values).astype('S')
