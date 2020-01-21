cd0e3ba71e0b6babbd01e95f8c00cd1aa2674227
Fionn Malone <f.malone13@imperial.ac.uk>
2020-01-20 18:28:16 -0800

    Fix generic test.

--- pauxy/systems/tests/test_generic.py
+++ pauxy/systems/tests/test_generic.py
@@ -3,6 +3,7 @@ import unittest
 import numpy
 import pytest
 from pauxy.systems.generic import Generic
+from pauxy.systems.utils import get_generic_integrals
 from pauxy.utils.testing import generate_hamiltonian
 
 
@@ -43,16 +44,26 @@ def test_read():
     numpy.random.seed(7)
     nmo = 13
     nelec = (4,3)
-    h1e, chol, enuc, eri = generate_hamiltonian(nmo, nelec, cplx=True, sym=4)
+    h1e_, chol_, enuc_, eri_ = generate_hamiltonian(nmo, nelec, cplx=True, sym=4)
     from pauxy.utils.io import write_qmcpack_dense
-    chol_ = chol.reshape((-1,nmo*nmo)).T.copy()
-    write_qmcpack_dense(h1e, chol_, nelec, nmo,
-                        enuc=enuc, filename='hamil.h5',
+    chol_ = chol_.reshape((-1,nmo*nmo)).T.copy()
+    write_qmcpack_dense(h1e_, chol_, nelec, nmo,
+                        enuc=enuc_, filename='hamil.h5',
                         real_chol=False)
-    options = {'nup': nelec[0], 'ndown': nelec[1], 'integrals': 'hamil.h5'}
-    sys = Generic(inputs=options)
-    schol = sys.chol_vecs
-    assert numpy.linalg.norm(chol-schol) == pytest.approx(0.0)
+    filename = 'hamil.h5'
+    nup, ndown = nelec
+    comm = None
+    hcore, chol, h1e_mod, enuc = get_generic_integrals(filename,
+                                                       comm=comm,
+                                                       verbose=False)
+    system = Generic(h1e=hcore, chol=chol, ecore=enuc,
+                     h1e_mod=h1e_mod, nelec=nelec,
+                     verbose=False)
+    assert system.ecore == pytest.approx(0.4392816555570978)
+    assert system.chol_vecs.shape == chol_.shape
+    assert len(system.H1.shape) == 3
+    assert numpy.linalg.norm(system.H1[0]-h1e_) == pytest.approx(0.0)
+    assert numpy.linalg.norm(system.chol_vecs-chol_) == pytest.approx(0.0)
 
 def teardown_module():
     cwd = os.getcwd()
--- pauxy/systems/utils.py
+++ pauxy/systems/utils.py
@@ -1,3 +1,5 @@
+import numpy
+import sys
 from pauxy.systems.hubbard import Hubbard
 from pauxy.systems.generic import Generic, read_integrals, construct_h1e_mod
 from pauxy.systems.ueg import UEG
@@ -21,11 +23,16 @@ def get_system(sys_opts=None, verbose=0, chol_cut=1e-5, comm=None):
     if sys_opts['name'] == 'Hubbard':
         system = Hubbard(sys_opts, verbose)
     elif sys_opts['name'] == 'Generic':
-        filename = sys_opts.get('integrals', False)
+        filename = sys_opts.get('integrals', None)
+        if filename is None:
+            if comm.rank == 0:
+                print("# Error: integrals not specfied.")
+                sys.exit()
         nup, ndown = sys_opts.get('nup'), sys_opts.get('ndown')
         if nup is None or ndown is None:
-            print("# Number of electrons not specified.")
-            sys.exit()
+            if comm.rank == 0:
+                print("# Error: Number of electrons not specified.")
+                sys.exit()
         nelec = (nup, ndown)
         hcore, chol, h1e_mod, enuc = get_generic_integrals(filename,
                                                            comm=comm,
@@ -68,14 +75,13 @@ def get_generic_integrals(filename, comm=None, verbose=False):
         if comm.rank == 0:
             chol_shmem[:] = chol[:]
         comm.Barrier()
-        # h1e_mod_shmem = get_shared_array(comm, hcore_shmem.shape, dtype)
-        # if comm.rank == 0:
-            # construct_h1e_mod(chol_shmem, hcore_shmem, h1e_mod_shmem)
-        # comm.Barrier()
-        h1e_mod_shmem = None
+        h1e_mod_shmem = get_shared_array(comm, hcore_shmem.shape, dtype)
+        if comm.rank == 0:
+            construct_h1e_mod(chol_shmem, hcore_shmem, h1e_mod_shmem)
+        comm.Barrier()
         return hcore_shmem, chol_shmem, h1e_mod_shmem, enuc
     else:
-        hcore, chol, enuc = read_integrals(filename, sparse)
+        hcore, chol, enuc = read_integrals(filename)
         h1 = numpy.array([hcore, hcore])
         h1e_mod = numpy.zeros(h1.shape, dtype=h1.dtype)
         construct_h1e_mod(chol, h1, h1e_mod)
