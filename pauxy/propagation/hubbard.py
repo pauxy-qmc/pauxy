5a81d9cbb03f53081f652a0e224475332e269682
Joonho Lee <joonholee@joonhos-imac.lan>
2020-02-07 12:31:03 -0500

    att hubb

--- pauxy/propagation/hubbard.py
+++ pauxy/propagation/hubbard.py
@@ -64,8 +64,10 @@ class HirschSpin(object):
             if verbose:
                 print("# Attractive U detected; charge decomposition is used")
             # field by spin
-            self.auxf = numpy.array([[numpy.exp(self.gamma) * numpy.exp(-0.5*self.gamma), numpy.exp(self.gamma) * numpy.exp(-0.5*self.gamma)],
-                                    [numpy.exp(-self.gamma) * numpy.exp(0.5*self.gamma), numpy.exp(-self.gamma)* numpy.exp(0.5*self.gamma)]])
+            # self.auxf = numpy.array([[numpy.exp(0.5*self.gamma), numpy.exp(self.gamma) * numpy.exp(-0.5*self.gamma)],
+            #                         [numpy.exp(-0.5*self.gamma), numpy.exp(-self.gamma)* numpy.exp(0.5*self.gamma)]])
+            self.auxf = numpy.array([[numpy.exp(self.gamma), numpy.exp(self.gamma)],
+                                    [numpy.exp(-self.gamma), numpy.exp(-self.gamma)]])
             self.auxf = self.auxf * numpy.exp(-0.5*qmc.dt*numpy.abs(system.U))
             # self.auxf = numpy.array([[numpy.exp(-0.5*self.gamma), numpy.exp(-0.5*self.gamma)],
             #                         [numpy.exp(0.5*self.gamma), numpy.exp(0.5*self.gamma)]])
