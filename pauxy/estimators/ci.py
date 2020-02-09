440aa8cafd8b2c3c388089eaf4dd514ef21fcfea
Joonho Lee <joonholee@joonhos-imac.lan>
2020-02-09 18:06:13 -0500

    lang firsov fci

--- pauxy/estimators/ci.py
+++ pauxy/estimators/ci.py
@@ -5,6 +5,389 @@ import scipy.linalg
 import scipy.sparse.linalg
 import itertools
 
+def decode_basis(nx, ny, i):
+    """Return cartesian lattice coordinates from basis index.
+
+    Consider a 3x3 lattice then we index lattice sites like::
+
+        (0,2) (1,2) (2,2)       6 7 8
+        (0,1) (1,1) (2,1)  ->   3 4 5
+        (0,0) (1,0) (2,0)       0 1 2
+
+    i.e., i = i_x + n_x * i_y, and i_x = i%n_x, i_y = i//nx.
+
+    Parameters
+    ----------
+    nx : int
+        Number of x lattice sites.
+    ny : int
+        Number of y lattice sites.
+    i : int
+        Basis index (same for up and down spins).
+    """
+    if ny == 1:
+        return numpy.array([i%nx])
+    else:
+        return numpy.array([i%nx, i//nx])
+
+def simple_lang_firsov_unitary(system, nboson_max = 1, gen_dets=False, occs=None, hamil=False):
+    """Very dumb FCI routine."""
+    orbs = numpy.arange(system.nbasis)
+
+    # bosons
+    blkboson = [1] # blk size for each boson sector
+    perms = [[0 for i in range(system.nbasis)]]
+    for nboson in range(1,nboson_max+1):
+        perm = list(unlabeled_balls_in_labeled_boxes(nboson, [nboson for i in range(system.nbasis)]))
+        perms += perm
+        blkboson += [len(perm)]
+
+    nperms = len(perms)
+    for i, perm in enumerate(perms):
+        perms[i] = numpy.array(perm)
+
+    if occs is None:
+        oa = [c for c in itertools.combinations(orbs, system.nup)]
+        ob = [c for c in itertools.combinations(orbs, system.ndown)]
+        oa, ob = zip(*itertools.product(oa,ob))
+    else:
+        oa, ob = occs
+
+    # convert to spin orbitals
+    dets = [[j for j in a] + [i+system.nbasis for i in c] for (a,c) in zip(oa,ob)]
+    dets = [numpy.sort(d) for d in dets]
+    ndets = len(dets)
+
+    print("# ndets, nperms, ntot = {}, {}, {}".format(ndets, nperms, ndets*nperms))
+
+    ntot = ndets*nperms
+    
+    # Htot = numpy.zeros((ndets*nperms, ndets*nperms))
+    Htot = scipy.sparse.csr_matrix((ndets*nperms, ndets*nperms))
+    
+    Iel = scipy.sparse.eye(ndets)
+    Ib = scipy.sparse.eye(nperms)
+
+    hel = scipy.sparse.csr_matrix((ndets,ndets))
+    for i in range(ndets):
+        for j in range(i,ndets):
+            hel[i,j] = get_hmatel(system, dets[i], dets[j])
+            hel[j,i] = hel[i,j]
+
+    print("# finshed forming hel")
+
+    hb = scipy.sparse.csr_matrix((nperms, nperms))
+
+    for i in range(nperms):
+        p = numpy.asarray(perms[i])
+        nocc = numpy.sum(p)
+        hb[i,i] = system.w0 * nocc
+
+    print("# finshed forming hb")
+
+    Heb = scipy.sparse.csr_matrix(Htot.shape)
+    for isite in range(system.nbasis):
+        rhoi = scipy.sparse.csr_matrix((ndets, ndets))
+        for i, di in enumerate(dets):
+            for d in di:
+                ii, spin_ii = map_orb(d, system.nbasis)
+                if (ii == isite):
+                    rhoi[i,i] += 1.0
+        
+        bi = scipy.sparse.csr_matrix((nperms, nperms))
+        for i, iperm in enumerate(perms):
+            ni = numpy.sum(iperm)
+            offset_i = numpy.sum(blkboson[:ni+1]) # block size sum
+            if (ni == nboson_max):
+                continue
+
+            for j, jperm in enumerate(perms[offset_i:offset_i+blkboson[ni+1]]):
+                diff = numpy.array(iperm) - numpy.array(jperm)
+                ndiff = numpy.sum(numpy.abs(diff))
+                if (ndiff == 1 and diff[isite] == -1):
+                    factor = math.sqrt(numpy.array(iperm)[isite]+1)
+                    bi[i,j+offset_i] = 1.0 * factor
+
+        xi = bi + bi.T
+
+        srhoi = scipy.sparse.csr_matrix(rhoi)
+        sxi = scipy.sparse.csr_matrix(xi)
+
+        Heb += system.g * scipy.sparse.kron(sxi, srhoi)
+    
+    print("# finshed forming Heb")
+
+    
+    He = scipy.sparse.kron(Ib, hel)
+    Hb = scipy.sparse.kron(hb, Iel)
+
+    Htot = He + Hb + Heb
+
+    U = scipy.sparse.csr_matrix((ndets*nperms, ndets*nperms))
+
+    for isite in range(system.nbasis):
+        rhoi = scipy.sparse.csr_matrix((ndets, ndets))
+        for i, di in enumerate(dets):
+            for d in di:
+                ii, spin_ii = map_orb(d, system.nbasis)
+                if (ii == isite):
+                    rhoi[i,i] += 1.0
+
+        bi = scipy.sparse.csr_matrix((nperms, nperms))
+        for i, iperm in enumerate(perms):
+            ni = numpy.sum(iperm)
+            offset_i = numpy.sum(blkboson[:ni+1]) # block size sum
+            if (ni == nboson_max):
+                continue
+
+            for j, jperm in enumerate(perms[offset_i:offset_i+blkboson[ni+1]]):
+                diff = numpy.array(iperm) - numpy.array(jperm)
+                ndiff = numpy.sum(numpy.abs(diff))
+                if (ndiff == 1 and diff[isite] == -1):
+                    factor = math.sqrt(numpy.array(iperm)[isite]+1)
+                    bi[i,j+offset_i] = 1.0 * factor
+
+        Hb = scipy.sparse.kron(hb, Iel)
+        
+        pi = bi.T - bi
+
+        const = system.gamma * numpy.sqrt(system.m * system.w0 / 2.0)
+        pi_tot = scipy.sparse.kron(pi, Iel)
+        ni_tot = scipy.sparse.kron(Ib, rhoi)
+
+        exp_nipi = scipy.sparse.linalg.expm(-const*pi_tot.multiply(ni_tot))
+
+        U += exp_nipi
+
+    U /= 2.0
+
+    identity = U.T.dot(U)
+    # print("identity = {}".format(identity))
+
+    # print("Htot before = {}".format(Htot.todense()))
+    Htot = U.T.dot(Htot).dot(U)
+    # print("Htot after = {}".format(Htot.todense()))
+
+    print("# finshed forming Htot")
+    print("# He nnz = {} out of total {}".format(He.nnz,ndets*nperms*ndets*nperms))
+    print("# Hb nnz = {} out of total {}".format(Hb.nnz,ndets*nperms*ndets*nperms))
+    print("# Heb nnz = {} out of total {}".format(Heb.nnz,ndets*nperms*ndets*nperms))
+    print("# Htot nnz = {} out of total {}".format(Htot.nnz,ndets*nperms*ndets*nperms))
+
+    # eigval, eigvec = scipy.sparse.linalg.eigsh(Htot, k=5, which='SA')
+    eigval, eigvec = scipy.sparse.linalg.eigsh(Htot, k=1, which='SA')
+
+    Eel = eigvec[:,0].T.conj().dot(He.dot(eigvec[:,0]))
+    Eb = eigvec[:,0].T.conj().dot(Hb.dot(eigvec[:,0]))
+    Eeb = eigvec[:,0].T.conj().dot(Heb.dot(eigvec[:,0]))
+
+    print("# Eel, Eb, Eeb, Etot = {}, {}, {}, {}".format(Eel, Eb, Eeb, Eel+Eb+Eeb))
+
+    if gen_dets:
+        return (eigval, eigvec), (dets,numpy.array(oa),numpy.array(ob))
+    elif hamil:
+        return (eigval, eigvec), Htot
+    else:
+        return (eigval, eigvec)
+
+
+def simple_lang_firsov(system, nboson_max = 1, gen_dets=False, occs=None, hamil=False):
+    assert (system.U == 0.0)
+    """Very dumb FCI routine."""
+    orbs = numpy.arange(system.nbasis)
+
+    # bosons
+    blkboson = [1] # blk size for each boson sector
+    perms = [[0 for i in range(system.nbasis)]]
+    for nboson in range(1,nboson_max+1):
+        perm = list(unlabeled_balls_in_labeled_boxes(nboson, [nboson for i in range(system.nbasis)]))
+        perms += perm
+        blkboson += [len(perm)]
+    
+    # print("blkboson = {}".format(blkboson))
+    nperms = len(perms)
+    for i, perm in enumerate(perms):
+        perms[i] = numpy.array(perm)
+
+    if occs is None:
+        oa = [c for c in itertools.combinations(orbs, system.nup)]
+        ob = [c for c in itertools.combinations(orbs, system.ndown)]
+        oa, ob = zip(*itertools.product(oa,ob))
+    else:
+        oa, ob = occs
+
+    # convert to spin orbitals
+    dets = [[j for j in a] + [i+system.nbasis for i in c] for (a,c) in zip(oa,ob)]
+    dets = [numpy.sort(d) for d in dets]
+    ndets = len(dets)
+
+    # print("# ndets, nperms, ntot = {}, {}, {}".format(ndets, nperms, ndets*nperms))
+
+    ntot = ndets*nperms
+    
+    Htot = scipy.sparse.csr_matrix((ndets*nperms, ndets*nperms))
+    
+    Iel = scipy.sparse.eye(ndets)
+    Ib = scipy.sparse.eye(nperms)
+
+
+    hel = scipy.sparse.csr_matrix((ndets,ndets))
+    for isite in range(system.nbasis):
+        rhoi = scipy.sparse.csr_matrix((ndets, ndets))
+        for i, di in enumerate(dets):
+            for d in di:
+                ii, spin_ii = map_orb(d, system.nbasis)
+                if (ii == isite):
+                    rhoi[i,i] += 1.0
+        const = system.gamma**2 * system.w0 / 2.0 - system.g * system.gamma * numpy.sqrt(2.0 * system.m * system.w0)
+        hel += const * scipy.sparse.csr_matrix(rhoi)
+
+    He = scipy.sparse.kron(Ib, hel)
+    
+    # print("# finshed forming he")
+
+    hb = scipy.sparse.csr_matrix((nperms, nperms))
+
+    for i in range(nperms):
+        p = numpy.asarray(perms[i])
+        nocc = numpy.sum(p)
+        hb[i,i] = system.w0 * nocc
+
+    # print("# finshed forming hb")
+
+
+    Heb = scipy.sparse.csr_matrix(Htot.shape)
+    for isite in range(system.nbasis):
+        bi = scipy.sparse.csr_matrix((nperms, nperms))
+        for i, iperm in enumerate(perms):
+            ni = numpy.sum(iperm)
+            offset_i = numpy.sum(blkboson[:ni+1]) # block size sum
+            if (ni == nboson_max):
+                continue
+
+            for j, jperm in enumerate(perms[offset_i:offset_i+blkboson[ni+1]]):
+                diff = numpy.array(iperm) - numpy.array(jperm)
+                ndiff = numpy.sum(numpy.abs(diff))
+                if (ndiff == 1 and diff[isite] == -1):
+                    factor = math.sqrt(numpy.array(iperm)[isite]+1)
+                    bi[i,j+offset_i] = 1.0 * factor
+
+        xy1 = decode_basis(system.nx, system.ny, isite)
+
+        for jsite in range(isite+1, system.nbasis):
+
+            xy2 = decode_basis(system.nx, system.ny, jsite)
+
+            dij = abs(xy1-xy2)
+
+            if sum(dij) == 1:
+                bj = scipy.sparse.csr_matrix((nperms, nperms))
+                for i, iperm in enumerate(perms):
+                    ni = numpy.sum(iperm)
+                    offset_i = numpy.sum(blkboson[:ni+1]) # block size sum
+                    if (ni == nboson_max):
+                        continue
+
+                    for j, jperm in enumerate(perms[offset_i:offset_i+blkboson[ni+1]]):
+                        diff = numpy.array(iperm) - numpy.array(jperm)
+                        ndiff = numpy.sum(numpy.abs(diff))
+                        if (ndiff == 1 and diff[jsite] == -1):
+                            factor = math.sqrt(numpy.array(iperm)[jsite]+1)
+                            bj[i,j+offset_i] = 1.0 * factor
+        
+                pi = bi.T - bi
+                pj = bj.T - bj
+
+                # print("pi = {}".format(pi.todense()))
+
+                const = system.gamma * numpy.sqrt(system.m * system.w0 / 2.0)
+                expij = scipy.sparse.linalg.expm(scipy.sparse.csr_matrix(pi-pj) * const)
+
+                hel = scipy.sparse.csr_matrix((ndets,ndets))
+                for idet in range(ndets):
+                    di = dets[idet]
+                    for jdet in range(idet+1, ndets):
+                        dj = dets[jdet]
+                        from_orb = list(set(dj)-set(di))
+                        to_orb = list(set(di)-set(dj))
+                        nex = len(from_orb)
+                        if (nex > 1 or nex == 0):
+                            continue
+                        else:
+                            i, si = map_orb(from_orb[0], system.nbasis)
+                            a, sa = map_orb(to_orb[0], system.nbasis)
+
+                            assert(si == sa)
+
+                            perm = get_perm(from_orb, to_orb, di, dj)
+
+                            if (perm):
+                                hel[idet,jdet] = -system.T[si][i,a]
+                            else:
+                                hel[idet,jdet] = system.T[si][i,a]
+
+
+                expij = scipy.sparse.linalg.expm(scipy.sparse.csr_matrix(pi-pj) * const)
+                identity = expij.T.dot(expij)
+
+                # print(identity)
+
+                Heb += scipy.sparse.kron(expij, hel)
+    
+    Heb = Heb + Heb.T
+
+    # print("# finshed forming Heb")
+
+    
+    Hb = scipy.sparse.kron(hb, Iel)
+    Htot = He + Hb + Heb
+
+    # print("# finshed forming Htot")
+    # print("# He nnz = {} out of total {}".format(He.nnz,ndets*nperms*ndets*nperms))
+    # print("# Hb nnz = {} out of total {}".format(Hb.nnz,ndets*nperms*ndets*nperms))
+    # print("# Heb nnz = {} out of total {}".format(Heb.nnz,ndets*nperms*ndets*nperms))
+    # print("# Htot nnz = {} out of total {}".format(Htot.nnz,ndets*nperms*ndets*nperms))
+
+    eigval, eigvec = scipy.sparse.linalg.eigsh(Htot, k=1, which='SA')
+
+    Eel = eigvec[:,0].T.conj().dot(He.dot(eigvec[:,0]))
+    Eb = eigvec[:,0].T.conj().dot(Hb.dot(eigvec[:,0]))
+    Eeb = eigvec[:,0].T.conj().dot(Heb.dot(eigvec[:,0]))
+
+    print("# Eel, Eb, Eeb, Etot = {}, {}, {}, {}".format(Eel, Eb, Eeb, Eel+Eb+Eeb))
+
+    for isite in range(system.nbasis):
+        bi = scipy.sparse.csr_matrix((nperms, nperms))
+        for i, iperm in enumerate(perms):
+            ni = numpy.sum(iperm)
+            offset_i = numpy.sum(blkboson[:ni+1]) # block size sum
+            if (ni == nboson_max):
+                continue
+
+            for j, jperm in enumerate(perms[offset_i:offset_i+blkboson[ni+1]]):
+                diff = numpy.array(iperm) - numpy.array(jperm)
+                ndiff = numpy.sum(numpy.abs(diff))
+                if (ndiff == 1 and diff[isite] == -1):
+                    factor = math.sqrt(numpy.array(iperm)[isite]+1)
+                    bi[i,j+offset_i] = 1.0 * factor
+
+        nib = bi.T.dot(bi)
+        ni = scipy.sparse.kron(nib, Iel)
+
+        nocc1 =  eigvec[:,0].T.conj().dot(ni.dot(eigvec[:,0]))
+        print("i, nocc1 = {}, {}".format(isite, nocc1))
+        # nocc2 =  eigvec[:,1].T.conj().dot(ni.dot(eigvec[:,1]))
+        # print("i, nocc1, nocc2 = {}, {}, {}".format(isite, nocc1, nocc2))
+
+
+
+    if gen_dets:
+        return (eigval, eigvec), (dets,numpy.array(oa),numpy.array(ob))
+    elif hamil:
+        return (eigval, eigvec), Htot
+    else:
+        return (eigval, eigvec)
+
 def simple_fci_bose_fermi(system, nboson_max = 1, gen_dets=False, occs=None, hamil=False):
     """Very dumb FCI routine."""
     orbs = numpy.arange(system.nbasis)
