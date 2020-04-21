abb4f8a959b1df174621218748ac930d97d31a9b
Joonho Lee <joonholee@joonhos-imac.lan>
2020-04-21 12:10:42 -0400

    sparse threshold added to the converter

--- tools/pyscf/pyscf_to_pauxy.py
+++ tools/pyscf/pyscf_to_pauxy.py
@@ -31,6 +31,8 @@ def parse_args(args):
                         default=1e-5, help='Cholesky convergence threshold.')
     parser.add_argument('-s', '--sparse', dest='sparse', action='store_true',
                         default=False, help='Write in sparse format.')
+    parser.add_argument('-sz', '--sparse_zero', dest='sparse_zero', type=float,
+                        default=1e-16, help='Sparsity threshold')
     parser.add_argument('-b', '--back-prop', dest='bp', action='store_true',
                         default=False, help='Add back propagation option to json'
                         'input file.')
@@ -59,7 +61,7 @@ def main(args):
     options = parse_args(args)
     dump_pauxy(chkfile=options.input_scf, hamil_file=options.output,
                wfn_file=options.wfn, chol_cut=options.thresh,
-               sparse=options.sparse, ortho_ao=options.oao)
+               sparse=options.sparse, sparse_zero=options.sparse_zero, ortho_ao=options.oao)
     write_input(options.json_input, options.output, options.wfn, options.bp)
 
 if __name__ == '__main__':
