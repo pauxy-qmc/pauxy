c4fb42593571a3e5f506f0c4da36b9e7d365f03a
Fionn Malone <fionn.malone@gmail.com>
2020-05-24 18:58:04 -0700

    git tag.

--- pauxy/__init__.py
+++ pauxy/__init__.py
@@ -1,4 +1,6 @@
 
 from ._version import get_versions
-__version__ = get_versions()['version']
+v = get_versions()
+__version__ = v.get("closest-tag", v["version"])
+__git_version__ = v.get("full-revisionid")
 del get_versions
