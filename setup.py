e4463d3cb19a292f03ab99df5c1588ec141bed10
Fionn Malone <fionn.malone@gmail.com>
2020-05-24 18:33:52 -0700

    Update setup.py>

--- setup.py
+++ setup.py
@@ -12,15 +12,13 @@ extensions = [
 
 setup(
     name='pauxy',
-    version='0.1',
-    author='Fionn Malone',
+    author='PAUXY developers',
     url='http://github.com/fdmalone/pauxy',
     packages=find_packages(exclude=['examples', 'docs', 'tests', 'tools', 'setup.py']),
     license='Lesser GPL v2.1',
     description='Python Implementations of Auxilliary Field QMC algorithms',
+    python_requires=">=3.6.0",
     long_description=open('README.rst').read(),
-    requires=['numpy (>=0.19.1)', 'pandas (>=0.20)',
-	          'scipy (>=1.13.3)', 'h5py (>=2.7.1)'],
     ext_modules = cythonize(extensions, include_path=[numpy.get_include()],
                             compiler_directives={'language_level':
                                                  sys.version_info[0]})
